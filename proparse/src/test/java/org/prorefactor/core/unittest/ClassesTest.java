80d1c58488db9f2aba6d279af7d69f30c64e1d97
Gilles QUERRET <g.querret@riverside-software.fr>
2016-07-21 16:00:49 +0200

    Fixed merge problem

--- proparse/src/test/java/org/prorefactor/core/unittest/ClassesTest.java
+++ proparse/src/test/java/org/prorefactor/core/unittest/ClassesTest.java
@@ -10,8 +10,10 @@
  *******************************************************************************/ 
 package org.prorefactor.core.unittest;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertNull;
+import static org.testng.Assert.assertTrue;
 
 import java.io.File;
 
