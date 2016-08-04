28427d0dbf825dcdd4464992df6f6e6375f02325
Gilles QUERRET <g.querret@riverside-software.fr>
2016-08-04 15:48:12 +0200

    Test case for CPD problem

---
+++ openedge-plugin/src/test/java/org/sonar/plugins/openedge/sensor/OpenEdgeProparseSensorTest.java
@@ -0,0 +1,64 @@
+/*
+ * OpenEdge plugin for SonarQube
+ * Copyright (C) 2016 Riverside Software
+ * contact AT riverside DASH software DOT fr
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.plugins.openedge.sensor;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import org.sonar.api.batch.fs.InputFile.Type;
+import org.sonar.api.batch.fs.internal.DefaultInputFile;
+import org.sonar.api.batch.sensor.internal.SensorContextTester;
+import org.sonar.api.internal.google.common.io.Files;
+import org.sonar.plugins.openedge.foundation.OpenEdge;
+import org.sonar.plugins.openedge.foundation.OpenEdgeComponents;
+import org.sonar.plugins.openedge.foundation.OpenEdgeSettings;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class OpenEdgeProparseSensorTest {
+  private final File moduleBaseDir = new File("src/test/resources/project1");
+  private final static String FILE3 = "src/procedures/test3.p";
+
+  @Test
+  public void testCPDPreprocessorExpansion() throws Exception {
+    SensorContextTester context = createContext();
+    TestServer server =new TestServer();
+    OpenEdgeSettings oeSettings = new OpenEdgeSettings(context.settings(), context.fileSystem());
+    OpenEdgeComponents components = new OpenEdgeComponents(context.activeRules(), server, null, null);
+    OpenEdgeProparseSensor sensor = new OpenEdgeProparseSensor(context.fileSystem(), context.activeRules(), oeSettings, components, server);
+    sensor.execute(context);
+    Assert.assertNotNull(context.cpdTokens("file3:src/procedures/test3.p"));
+    Assert.assertEquals(context.cpdTokens("file3:src/procedures/test3.p").size(), 2);
+  }
+
+  private SensorContextTester createContext() throws IOException {
+    SensorContextTester context = SensorContextTester.create(moduleBaseDir);
+    context.settings().setProperty("sonar.sources", "src");
+    context.settings().setProperty("sonar.oe.binaries", "build");
+    context.fileSystem().add(
+        new DefaultInputFile("file3", FILE3).setLanguage(OpenEdge.KEY).setType(Type.MAIN).initMetadata(
+            Files.toString(new File(moduleBaseDir, FILE3), Charset.defaultCharset())));
+
+    return context;
+  }
+
+  
+}
---
+++ openedge-plugin/src/test/java/org/sonar/plugins/openedge/sensor/TestServer.java
@@ -0,0 +1,65 @@
+package org.sonar.plugins.openedge.sensor;
+
+import java.io.File;
+import java.util.Date;
+
+import org.sonar.api.platform.Server;
+
+public class TestServer extends Server {
+
+  @Override
+  public String getId() {
+    return null;
+  }
+
+  @Override
+  public String getVersion() {
+    return null;
+  }
+
+  @Override
+  public Date getStartedAt() {
+    return null;
+  }
+
+  @Override
+  public File getRootDir() {
+    return null;
+  }
+
+  @Override
+  public File getDeployDir() {
+    return null;
+  }
+
+  @Override
+  public String getContextPath() {
+    return null;
+  }
+
+  @Override
+  public String getPublicRootUrl() {
+    return null;
+  }
+
+  @Override
+  public boolean isDev() {
+    return false;
+  }
+
+  @Override
+  public boolean isSecured() {
+    return false;
+  }
+
+  @Override
+  public String getURL() {
+    return null;
+  }
+
+  @Override
+  public String getPermanentServerId() {
+    return "";
+  }
+
+}
