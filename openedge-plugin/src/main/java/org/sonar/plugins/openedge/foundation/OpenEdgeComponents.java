f5e1fba32e49401afa148a1229fe9a93aeaa1f5f
Gilles QUERRET <g.querret@riverside-software.fr>
2017-02-23 12:42:49 +0100

    Import

--- openedge-plugin/src/main/java/org/sonar/plugins/openedge/foundation/OpenEdgeComponents.java
+++ openedge-plugin/src/main/java/org/sonar/plugins/openedge/foundation/OpenEdgeComponents.java
@@ -34,7 +34,6 @@ import org.apache.commons.lang.StringUtils;
 import org.sonar.api.batch.BatchSide;
 import org.sonar.api.batch.rule.ActiveRule;
 import org.sonar.api.batch.sensor.SensorContext;
-import org.sonar.api.platform.Server;
 import org.sonar.api.rule.RuleKey;
 import org.sonar.api.utils.MessageException;
 import org.sonar.api.utils.log.Logger;
