0ba662c826cb4360e80599252823c444ce712f15
Gilles QUERRET <g.querret@riverside-software.fr>
2017-05-02 13:14:32 +0200

    Test

--- database-parser/src/test/java/eu/rssw/antlr/database/TestDumpFile.java
+++ database-parser/src/test/java/eu/rssw/antlr/database/TestDumpFile.java
@@ -8,15 +8,9 @@ import static org.testng.Assert.assertTrue;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
 
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -73,48 +67,21 @@ public class TestDumpFile {
   }
 
   @Test
-  public void testSerialize() throws IOException, ClassNotFoundException {
+  public void testSerialize() throws IOException {
     DatabaseDescription db = DumpFileUtils.getDatabaseDescription(new File("src/test/resources/sp2k.df"));
     // Serialize object
     ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-    ObjectOutput out = new ObjectOutputStream(bytes);
-    out.writeObject(db);
+    db.serialize(bytes);
     // Deserialize
     InputStream in = new ByteArrayInputStream(bytes.toByteArray());
-    ObjectInputStream stream = new ObjectInputStream(in);
-    DatabaseDescription db2 = (DatabaseDescription) stream.readObject();
+    DatabaseDescription db2 = DatabaseDescription.deserialize(in, "sp2k");
     // Compare
     Assert.assertEquals(db2.getSequences().size(), db.getSequences().size());
     Assert.assertEquals(db2.getTables().size(), db.getTables().size());
-  }
-
-  @Test
-  public void testLargeFile() throws IOException {
-
-    DumpFileGrammarLexer lexer1 = new DumpFileGrammarLexer(CharStreams.fromFileName("src/test/resources/sp2k.df"));
-    CommonTokenStream tokens1 = new CommonTokenStream(lexer1);
-    DumpFileGrammarParser parser1 = new DumpFileGrammarParser(tokens1);
-    parser1.dump();
-
-    // 1 table : 0,166
-    // 10 tables : 0,175
-    long t1 = System.currentTimeMillis();
-    DumpFileGrammarLexer lexer = new DumpFileGrammarLexer(CharStreams.fromFileName("src/test/resources/vision.df"));
-    CommonTokenStream tokens = new CommonTokenStream(lexer);
-    DumpFileGrammarParser parser = new DumpFileGrammarParser(tokens);
-    // parser.getInterpreter().setPredictionMode(PredictionMode.SLL);
-    // parser.dump();
-    
-    DumpFileVisitor v = new DumpFileVisitor("plop");
-    v.visit(parser.dump());
-    System.out.println("Time " + (System.currentTimeMillis() - t1));
-    
-    try (FileOutputStream fichierOut = new FileOutputStream("maclasse.ser"); 
-    ObjectOutputStream oos = new ObjectOutputStream(fichierOut)) {
-      oos.writeObject(v.getDatabase());
-    
-    }
-    
+    Assert.assertEquals(db2.getTable("BillTo").getField("Address").getDataType().toUpperCase(), "CHARACTER");
+    Assert.assertEquals(db2.getTable("Bin").getField("Qty").getDataType().toUpperCase(), "INTEGER");
+    Assert.assertEquals(db2.getTable("Customer").getField("CreditLimit").getDataType().toUpperCase(), "DECIMAL");
+    Assert.assertEquals(db2.getTable("Salesrep").getField("monthquota").getExtent().intValue(), 12);
   }
 
 }
