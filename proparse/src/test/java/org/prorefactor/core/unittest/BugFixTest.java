dc9512f7ccbff0b9731eb688e8c16df6531490e6
Gilles QUERRET <g.querret@riverside-software.fr>
2017-08-01 08:47:51 +0200

    Test case

--- proparse/src/test/java/org/prorefactor/core/unittest/BugFixTest.java
+++ proparse/src/test/java/org/prorefactor/core/unittest/BugFixTest.java
@@ -356,6 +356,11 @@ public class BugFixTest {
   }
 
   @Test
+  public void testOnEvent() throws ANTLRException {
+    genericTest("onEvent.p");
+  }
+
+  @Test
   public void testGetDbClient() throws ANTLRException {
     genericTest("getdbclient.p");
   }
