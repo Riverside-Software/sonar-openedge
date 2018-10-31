2002363b8a50b54caae93b89b1e378061ce12c7d
Gilles QUERRET <g.querret@riverside-software.fr>
2018-10-31 14:51:32 +0100

    Comments

--- rcode-reader/src/main/java/eu/rssw/pct/elements/ITypeInfoBuilder.java
+++
@@ -1,7 +0,0 @@
-package eu.rssw.pct.elements;
-
-public interface ITypeInfoBuilder {
-  ITypeInfoBuilder newTypeInfoBuilder(byte[] segment);
-  ITypeInfoBuilder newMethod();
-  ITypeInfo build();
-}
