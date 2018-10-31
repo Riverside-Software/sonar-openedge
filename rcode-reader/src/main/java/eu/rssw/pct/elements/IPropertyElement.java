ae32b610bc88f904ae077111ac886e26f06e0160
Gilles QUERRET <g.querret@riverside-software.fr>
2018-10-31 14:34:04 +0100

    Sonar

--- rcode-reader/src/main/java/eu/rssw/pct/elements/IPropertyElement.java
+++ rcode-reader/src/main/java/eu/rssw/pct/elements/IPropertyElement.java
@@ -2,21 +2,6 @@ package eu.rssw.pct.elements;
 
 
 public interface IPropertyElement extends IAccessibleElement {
-  // NO-SONAR
-  public static final int PUBLIC_GETTER = 1;
-  public static final int PROTECTED_GETTER = 2;
-  public static final int PRIVATE_GETTER = 4;
-  public static final int PUBLIC_SETTER = 8;
-  public static final int PROTECTED_SETTER = 16;
-  public static final int PRIVATE_SETTER = 32;
-  public static final int HAS_GETTER = 256;
-  public static final int HAS_SETTER = 512;
-  public static final int PROPERTY_AS_VARIABLE = 1024;
-  public static final int PROPERTY_IS_INDEXED = 8192;
-  public static final int PROPERTY_IS_DEFAULT = 16384;
-  public static final int PROPERTY_IS_ENUM = 65536;
-  // NO-SONAR-END
-
   IVariableElement getVariable();
   IMethodElement getGetter();
   IMethodElement getSetter();
--- rcode-reader/src/main/java/eu/rssw/pct/elements/v11/PropertyElementV11.java
+++ rcode-reader/src/main/java/eu/rssw/pct/elements/v11/PropertyElementV11.java
@@ -32,6 +32,18 @@ import eu.rssw.pct.elements.IPropertyElement;
 import eu.rssw.pct.elements.IVariableElement;
 
 public class PropertyElementV11 extends AbstractAccessibleElement implements IPropertyElement {
+  public static final int PUBLIC_GETTER = 1;
+  public static final int PROTECTED_GETTER = 2;
+  public static final int PRIVATE_GETTER = 4;
+  public static final int PUBLIC_SETTER = 8;
+  public static final int PROTECTED_SETTER = 16;
+  public static final int PRIVATE_SETTER = 32;
+  public static final int HAS_GETTER = 256;
+  public static final int HAS_SETTER = 512;
+  public static final int PROPERTY_AS_VARIABLE = 1024;
+  public static final int PROPERTY_IS_INDEXED = 8192;
+  public static final int PROPERTY_IS_DEFAULT = 16384;
+  public static final int PROPERTY_IS_ENUM = 65536;
 
   private final int flags;
   private final IVariableElement variable;
