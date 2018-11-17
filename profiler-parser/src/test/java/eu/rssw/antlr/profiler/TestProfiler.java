5effce421c4094a08d8647c03ce72222badd8c71
Gilles QUERRET <g.querret@riverside-software.fr>
2018-11-17 11:56:52 +0100

    Remove unused import statements

--- profiler-parser/src/test/java/eu/rssw/antlr/profiler/TestProfiler.java
+++ profiler-parser/src/test/java/eu/rssw/antlr/profiler/TestProfiler.java
@@ -21,11 +21,6 @@ package eu.rssw.antlr.profiler;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.FileVisitor;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Map;
