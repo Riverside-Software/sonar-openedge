c58d654910e3960d2bfac3331875a518e45868fb
Gilles Querret <g.querret@riverside-software.fr>
2020-05-28 18:20:01 +0200

    Don't report issue if warning is not active in quality profile

--- openedge-plugin/src/test/java/org/sonar/plugins/openedge/sensor/OpenEdgeWarningsSensorTest.java
+++ openedge-plugin/src/test/java/org/sonar/plugins/openedge/sensor/OpenEdgeWarningsSensorTest.java
@@ -20,6 +20,7 @@
 package org.sonar.plugins.openedge.sensor;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.Iterator;
@@ -47,8 +48,53 @@ public class OpenEdgeWarningsSensorTest {
   @Test
   public void testWarnings() throws IOException {
     SensorContextTester context = TestProjectSensorContext.createContext();
-    context.setActiveRules(createRules());
-    OpenEdgeSettings oeSettings = new OpenEdgeSettings(context.config(), context.fileSystem(), SonarRuntimeImpl.forSonarQube(VERSION, SonarQubeSide.SCANNER, SonarEdition.COMMUNITY));
+    context.setActiveRules(createRules1());
+    OpenEdgeSettings oeSettings = new OpenEdgeSettings(context.config(), context.fileSystem(),
+        SonarRuntimeImpl.forSonarQube(VERSION, SonarQubeSide.SCANNER, SonarEdition.COMMUNITY));
+    OpenEdgeWarningsSensor sensor = new OpenEdgeWarningsSensor(oeSettings);
+    sensor.execute(context);
+
+    // Case-sensitive, so one issue can't be reported
+    Assert.assertEquals(context.allIssues().size(), 4);
+    Iterator<Issue> issues = context.allIssues().iterator();
+    Issue issue;
+
+    // Starts with ../
+    issue = issues.next();
+    assertEquals(issue.primaryLocation().inputComponent().key(),
+        TestProjectSensorContext.BASEDIR + ":" + TestProjectSensorContext.FILE4);
+    assertEquals(issue.primaryLocation().textRange().start().line(), 2);
+
+    issue = issues.next();
+    assertEquals(issue.primaryLocation().inputComponent().key(),
+        TestProjectSensorContext.BASEDIR + ":" + TestProjectSensorContext.FILE4);
+    assertEquals(issue.primaryLocation().textRange().start().line(), 3);
+    assertEquals(issue.ruleKey(),
+        RuleKey.of(Constants.STD_REPOSITORY_KEY, OpenEdgeRulesDefinition.COMPILER_WARNING_1688_RULEKEY));
+
+    issue = issues.next();
+    assertEquals(issue.primaryLocation().inputComponent().key(),
+        TestProjectSensorContext.BASEDIR + ":" + TestProjectSensorContext.FILE4);
+    assertEquals(issue.primaryLocation().textRange().start().line(), 3);
+    assertTrue(issue.primaryLocation().message().endsWith("Another one (1689)"));
+    assertEquals(issue.ruleKey(),
+        RuleKey.of(Constants.STD_REPOSITORY_KEY, OpenEdgeRulesDefinition.COMPILER_WARNING_RULEKEY));
+
+    issue = issues.next();
+    assertEquals(issue.primaryLocation().inputComponent().key(),
+        TestProjectSensorContext.BASEDIR + ":" + TestProjectSensorContext.FILE1);
+    assertEquals(issue.primaryLocation().textRange().start().line(), 1);
+    // Verify that leading 'WARNING' is removed, as well as the message number
+    assertEquals(issue.primaryLocation().message(),
+        "Program src\\procedures\\sample\\inc\\test.i, Line 1 is an expression statement that evaluates to a constant.");
+  }
+
+  @Test
+  public void testWarnings02() throws IOException {
+    SensorContextTester context = TestProjectSensorContext.createContext();
+    context.setActiveRules(createRules2());
+    OpenEdgeSettings oeSettings = new OpenEdgeSettings(context.config(), context.fileSystem(),
+        SonarRuntimeImpl.forSonarQube(VERSION, SonarQubeSide.SCANNER, SonarEdition.COMMUNITY));
     OpenEdgeWarningsSensor sensor = new OpenEdgeWarningsSensor(oeSettings);
     sensor.execute(context);
 
@@ -67,6 +113,9 @@ public class OpenEdgeWarningsSensorTest {
     assertEquals(issue.primaryLocation().inputComponent().key(),
         TestProjectSensorContext.BASEDIR + ":" + TestProjectSensorContext.FILE4);
     assertEquals(issue.primaryLocation().textRange().start().line(), 3);
+    assertTrue(issue.primaryLocation().message().endsWith("Another one (1689)"));
+    assertEquals(issue.ruleKey(),
+        RuleKey.of(Constants.STD_REPOSITORY_KEY, OpenEdgeRulesDefinition.COMPILER_WARNING_RULEKEY));
 
     issue = issues.next();
     assertEquals(issue.primaryLocation().inputComponent().key(),
@@ -77,10 +126,47 @@ public class OpenEdgeWarningsSensorTest {
         "Program src\\procedures\\sample\\inc\\test.i, Line 1 is an expression statement that evaluates to a constant.");
   }
 
-  private ActiveRules createRules() {
-    return new ActiveRulesBuilder().addRule(new NewActiveRule.Builder().setRuleKey(
+  @Test
+  public void testWarningsDeactivated() throws IOException {
+    SensorContextTester context = TestProjectSensorContext.createContext();
+    context.setActiveRules(createRules0());
+    OpenEdgeSettings oeSettings = new OpenEdgeSettings(context.config(), context.fileSystem(),
+        SonarRuntimeImpl.forSonarQube(VERSION, SonarQubeSide.SCANNER, SonarEdition.COMMUNITY));
+    OpenEdgeWarningsSensor sensor = new OpenEdgeWarningsSensor(oeSettings);
+    sensor.execute(context);
+
+    // Nothing should be reported
+    Assert.assertEquals(context.allIssues().size(), 0);
+  }
+
+  private ActiveRules createRules0() {
+    ActiveRulesBuilder builder = new ActiveRulesBuilder();
+    return builder.build();
+  }
+
+  private ActiveRules createRules1() {
+    ActiveRulesBuilder builder = new ActiveRulesBuilder();
+    builder.addRule(new NewActiveRule.Builder().setRuleKey(
         RuleKey.of(Constants.STD_REPOSITORY_KEY, OpenEdgeRulesDefinition.COMPILER_WARNING_RULEKEY)).setLanguage(
-            Constants.LANGUAGE_KEY).build()).build();
+            Constants.LANGUAGE_KEY).build());
+    builder.addRule(new NewActiveRule.Builder().setRuleKey(
+        RuleKey.of(Constants.STD_REPOSITORY_KEY, OpenEdgeRulesDefinition.COMPILER_WARNING_12115_RULEKEY)).setLanguage(
+            Constants.LANGUAGE_KEY).build());
+    builder.addRule(new NewActiveRule.Builder().setRuleKey(
+        RuleKey.of(Constants.STD_REPOSITORY_KEY, OpenEdgeRulesDefinition.COMPILER_WARNING_1688_RULEKEY)).setLanguage(
+            Constants.LANGUAGE_KEY).build());
+    return builder.build();
+  }
+
+  private ActiveRules createRules2() {
+    ActiveRulesBuilder builder = new ActiveRulesBuilder();
+    builder.addRule(new NewActiveRule.Builder().setRuleKey(
+        RuleKey.of(Constants.STD_REPOSITORY_KEY, OpenEdgeRulesDefinition.COMPILER_WARNING_RULEKEY)).setLanguage(
+            Constants.LANGUAGE_KEY).build());
+    builder.addRule(new NewActiveRule.Builder().setRuleKey(
+        RuleKey.of(Constants.STD_REPOSITORY_KEY, OpenEdgeRulesDefinition.COMPILER_WARNING_12115_RULEKEY)).setLanguage(
+            Constants.LANGUAGE_KEY).build());
+    return builder.build();
   }
 
 }
