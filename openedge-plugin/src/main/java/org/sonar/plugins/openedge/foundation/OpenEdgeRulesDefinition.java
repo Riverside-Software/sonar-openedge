ad2af89405f35f3d63776cdd52785b005770c361
Gilles Querret <g.querret@riverside-software.fr>
2020-10-01 10:09:40 +0200

    Remove remediation cost on Proparse issue

--- openedge-plugin/src/main/java/org/sonar/plugins/openedge/foundation/OpenEdgeRulesDefinition.java
+++ openedge-plugin/src/main/java/org/sonar/plugins/openedge/foundation/OpenEdgeRulesDefinition.java
@@ -95,7 +95,6 @@ public class OpenEdgeRulesDefinition implements RulesDefinition {
     // Manually created rule for proparse errors
     NewRule proparseRule = repository.createRule(PROPARSE_ERROR_RULEKEY).setName("Proparse error").setSeverity(
         Priority.INFO.name());
-    proparseRule.setDebtRemediationFunction(proparseRule.debtRemediationFunctions().constantPerIssue("3h"));
     proparseRule.setType(RuleType.BUG);
     proparseRule.setHtmlDescription(getClass().getResource(String.format(HTML_DOC_PATH, Constants.LANGUAGE_KEY,
         Constants.STD_REPOSITORY_KEY, proparseRule.key())));
