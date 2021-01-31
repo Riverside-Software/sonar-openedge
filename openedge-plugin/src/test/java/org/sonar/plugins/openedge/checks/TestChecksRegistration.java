b8b191f439c9cdcfb07352d2ab0891223198cb39
Gilles Querret <g.querret@riverside-software.fr>
2021-01-31 10:12:09 +0100

    Additional unit tests

---
+++ openedge-plugin/src/test/java/org/sonar/plugins/openedge/checks/TestChecksRegistration.java
@@ -0,0 +1,63 @@
+/*
+ * OpenEdge plugin for SonarQube
+ * Copyright (c) 2015-2021 Riverside Software
+ * contact AT riverside DASH software DOT fr
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+package org.sonar.plugins.openedge.checks;
+
+import org.sonar.api.utils.log.Logger;
+import org.sonar.api.utils.log.Loggers;
+import org.sonar.plugins.openedge.api.CheckRegistration;
+import org.sonar.plugins.openedge.api.checks.OpenEdgeDumpFileCheck;
+import org.sonar.plugins.openedge.api.checks.OpenEdgeProparseCheck;
+
+public class TestChecksRegistration implements CheckRegistration {
+  private static final Logger LOGGER = Loggers.get(TestChecksRegistration.class);
+
+  /**
+   * Register the classes that will be used to instantiate checks during analysis.
+   */
+  @Override
+  public void register(Registrar registrar) {
+    LOGGER.debug("Registering CheckRegistrar {}", TestChecksRegistration.class.toString());
+
+    for (Class<? extends OpenEdgeProparseCheck> clz : ppCheckClasses()) {
+      registrar.registerParserCheck(clz);
+    }
+    for (Class<? extends OpenEdgeDumpFileCheck> clz : dbCheckClasses()) {
+      registrar.registerDumpFileCheck(clz);
+    }
+  }
+
+  /**
+   * Lists all the proparse checks provided by the plugin
+   */
+  @SuppressWarnings("unchecked")
+  public static Class<? extends OpenEdgeProparseCheck>[] ppCheckClasses() {
+    return new Class[] {SharedObjectsAnalyzer.class, LargeTransactionScope.class, ClumsySyntax.class, IntegerRule.class};
+  }
+
+  /**
+   * Lists all the DB checks provided by the plugin
+   */
+  @SuppressWarnings("unchecked")
+  public static Class<? extends OpenEdgeDumpFileCheck>[] dbCheckClasses() {
+    return new Class[] {NoOpDatabaseRule.class};
+  }
+
+}
