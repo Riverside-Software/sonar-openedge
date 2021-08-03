Riverside-Software/sonar-openedge

commit c7f4bd07423b3f0adb7bb304428bb3099d4bc656

ExpressionTerm refactor in Proparse

proparse/src/main/antlr4/org/prorefactor/proparse/antlr4/Proparse.g4
@@ -276,6 +276,7 @@ statement:
   |  showStatsStatement
   |  statusStatement
   |  stopStatement
+  |  superStatement
   |  subscribeStatement
   |  systemDialogColorStatement
   |  systemDialogFontStatement
@@ -395,7 +396,6 @@ builtinFunction:
   |  PAGENUMBER LEFTPAREN streamname RIGHTPAREN  // also noarg
   |  PAGESIZE LEFTPAREN streamname RIGHTPAREN  // also noarg
   |  SEEK LEFTPAREN ( INPUT | OUTPUT | streamname | STREAMHANDLE expression ) RIGHTPAREN // streamname, /not/ stream_name_or_handle.
-  |  SUPER parameterList  // also noarg
   |  TYPEOF LEFTPAREN expression COMMA typeName RIGHTPAREN
   |  argFunction
   |  optionalArgFunction
@@ -509,16 +509,12 @@ expression:
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 expressionTerm:
-    NORETURNVALUE sWidget colonAttribute  # exprtNoReturnValue
-  | // Widget attributes has to be checked before field or func, because they can be ambiguous up to the OBJCOLON. Think about no-arg functions like SUPER.
-    // Also has to be checked before systemhandlename, because you want to pick up all of FILE-INFO:FILE-TYPE rather than just FILE-INFO, for example.
-    widName colonAttribute           # exprtWidName
-  | expressionTerm2 colonAttribute?  # exprtExprt2
-  ;
-
-widattr:
-    widName colonAttribute           # widattrWidName
-  | expressionTerm2 colonAttribute   # widattrExprt2
+    expressionTerm ( OBJCOLON | DOUBLECOLON ) id=nonPunctuating methodParamList  # exprTermMethodCall
+  | expressionTerm ( OBJCOLON | DOUBLECOLON ) id=nonPunctuating                  # exprTermAttribute
+  | expressionTerm arraySubscript # exprTermArray
+  | expressionTerm inuic          # exprTermInUI
+  | widName                       # exprTermWidget
+  | expressionTerm2               # exprTermOther
   ;
 
 expressionTerm2:
@@ -541,7 +537,6 @@ expressionTerm2:
     { support.isClass() && support.unknownMethodCallsAllowed() }? methodname=identifier parameterListNoRoot # exprt2ParenCall2
   | constant   # exprt2Constant
   | noArgFunction  # exprt2NoArgFunc
-  | systemHandleName  # exprt2SystemHandleName
   | field ( NOT? ENTERED )?  # exprt2Field
   ;
 
@@ -556,20 +551,12 @@ widName:
   | MENUITEM identifier
   | BROWSE identifier
   | QUERY identifier
-  | TEMPTABLE filn
-  | BUFFER filn
-  | XDOCUMENT filn
-  | XNODEREF filn
-  | SOCKET filn
+  | TEMPTABLE identifier
+  | BUFFER identifier
+  | XDOCUMENT identifier
+  | XNODEREF identifier
+  | SOCKET identifier
   | STREAM streamname
-  ;
-
-colonAttribute:
-    colonAttributeSub+ inuic? ( AS . )?
-  ;
-
-colonAttributeSub:
-    ( OBJCOLON | DOUBLECOLON ) id=. arraySubscript? methodParamList?
   ;
 
 gWidget:
@@ -583,7 +570,6 @@ widgetList:
 sWidget:
     widName | field
   ;
-
 
 filn:
     t1=identifier ( NAMEDOT t2=identifier )?
@@ -797,11 +783,11 @@ assignmentList: // SEMITRANSLATED
   ;
 
 assignStatement2:
-    ( pseudoFunction | widattr | field ) ( EQUAL | PLUSEQUAL | MINUSEQUAL | STAREQUAL | SLASHEQUAL ) expression NOERROR? statementEnd
+    ( pseudoFunction | expressionTerm | field ) ( EQUAL | PLUSEQUAL | MINUSEQUAL | STAREQUAL | SLASHEQUAL ) expression NOERROR? statementEnd
   ;
 
 assignEqual:
-   ( pseudoFunction | widattr | field ) ( EQUAL | PLUSEQUAL | MINUSEQUAL | STAREQUAL | SLASHEQUAL ) expression
+   ( pseudoFunction | expressionTerm | field ) ( EQUAL | PLUSEQUAL | MINUSEQUAL | STAREQUAL | SLASHEQUAL ) expression
   ;
 
 assignField:
@@ -1957,7 +1943,7 @@ dynamicPropertyFunction:
   ;
 
 fieldEqualDynamicNew:
-    (widattr | field) EQUAL dynamicNew
+    ( expressionTerm | field ) EQUAL dynamicNew
   ;
 
 dynamicNew:
@@ -3229,6 +3215,10 @@ stopAfter:
 
 stopStatement:
     STOP statementEnd
+  ;
+
+superStatement:
+    SUPER LEFTPAREN ( parameter ( COMMA parameter )* )? RIGHTPAREN
   ;
 
 streamNameOrHandle:

proparse/src/main/java/org/prorefactor/proparse/JPNodeVisitor.java
@@ -26,8 +26,8 @@ import org.antlr.v4.runtime.tree.TerminalNode;
 import org.prorefactor.core.ABLNodeType;
 import org.prorefactor.core.JPNode.Builder;
 import org.prorefactor.core.ProToken;
-import org.prorefactor.proparse.antlr4.ProparseBaseVisitor;
 import org.prorefactor.proparse.antlr4.Proparse.*;
+import org.prorefactor.proparse.antlr4.ProparseBaseVisitor;
 import org.prorefactor.proparse.support.ParserSupport;
 
 public class JPNodeVisitor extends ProparseBaseVisitor<Builder> {
@@ -312,31 +312,21 @@ public class JPNodeVisitor extends ProparseBaseVisitor<Builder> {
   // ---------------
 
   @Override
-  public Builder visitExprtNoReturnValue(ExprtNoReturnValueContext ctx) {
-    return createTree(ctx, ABLNodeType.WIDGET_REF);
+  public Builder visitExprTermMethodCall(ExprTermMethodCallContext ctx) {
+    return createTree(ctx, ABLNodeType.LOCAL_METHOD_REF).setRuleNode(ctx);
   }
 
   @Override
-  public Builder visitExprtWidName(ExprtWidNameContext ctx) {
-    return createTree(ctx, ABLNodeType.WIDGET_REF).setRuleNode(ctx);
+  public Builder visitExprTermAttribute(ExprTermAttributeContext ctx) {
+    return createTree(ctx, ABLNodeType.ATTRIBUTE_REF).setRuleNode(ctx);
   }
-
+  
   @Override
-  public Builder visitExprtExprt2(ExprtExprt2Context ctx) {
-    if (ctx.colonAttribute() != null) {
-      return createTree(ctx, ABLNodeType.WIDGET_REF);
-    }
-    return visitChildren(ctx);
-  }
-
-  @Override
-  public Builder visitWidattrWidName(WidattrWidNameContext ctx) {
-    return createTree(ctx, ABLNodeType.WIDGET_REF);
-  }
-
-  @Override
-  public Builder visitWidattrExprt2(WidattrExprt2Context ctx) {
-    return createTree(ctx, ABLNodeType.WIDGET_REF);
+  public Builder visitExprTermWidget(ExprTermWidgetContext ctx) {
+    if (ctx.widName().systemHandleName() == null)
+      return createTree(ctx, ABLNodeType.WIDGET_REF).setRuleNode(ctx);
+    else
+      return visitChildren(ctx).setRuleNode(ctx);
   }
 
   @Override
@@ -374,16 +364,6 @@ public class JPNodeVisitor extends ProparseBaseVisitor<Builder> {
   @Override
   public Builder visitGWidget(GWidgetContext ctx) {
     return createTree(ctx, ABLNodeType.WIDGET_REF);
-  }
-
-  @Override
-  public Builder visitFiln(FilnContext ctx) {
-    return visitChildren(ctx);
-  }
-
-  @Override
-  public Builder visitFieldn(FieldnContext ctx) {
-    return visitChildren(ctx);
   }
 
   @Override
@@ -565,15 +545,23 @@ public class JPNodeVisitor extends ProparseBaseVisitor<Builder> {
 
   @Override
   public Builder visitAssignStatement2(AssignStatement2Context ctx) {
-    // Unset rule node, as it's in fact assigned to the parent node
     Builder node1 = createTreeFromSecondNode(ctx).setOperator().unsetRuleNode();
 
+    // It is faster and easier to transform ASSIGN statement to EXPRESSION statement when the left-part
+    // of the assignment is a constant, rather than doing that directly in the grammar
+    if ((ctx.EQUAL() != null) && (ctx.expressionTerm() instanceof ExprTermOtherContext)) {
+      ExprTermOtherContext ctx2 = (ExprTermOtherContext) ctx.expressionTerm();
+      if (ctx2.expressionTerm2() instanceof Exprt2ConstantContext) {
+        return new Builder(ABLNodeType.EXPR_STATEMENT).setStatement().setDown(
+            node1.changeType(ABLNodeType.EQ)).setRuleNode(ctx);
+      }
+    }
+    // Unset rule node, as it's in fact assigned to the parent node
     Builder holder = new Builder(ABLNodeType.ASSIGN).setStatement().setDown(node1).setRuleNode(ctx);
     Builder lastNode = node1;
     for (int zz = 3; zz < ctx.getChildCount(); zz++) {
       lastNode = lastNode.setRight(visit(ctx.getChild(zz))).getLast();
     }
-
     return holder;
   }
 
@@ -691,11 +679,6 @@ public class JPNodeVisitor extends ProparseBaseVisitor<Builder> {
   @Override
   public Builder visitCaseWhen(CaseWhenContext ctx) {
     return createTreeFromFirstNode(ctx);
-  }
-
-  @Override
-  public Builder visitCaseExpression1(CaseExpression1Context ctx) {
-    return visitChildren(ctx);
   }
 
   @Override
@@ -883,11 +866,6 @@ public class JPNodeVisitor extends ProparseBaseVisitor<Builder> {
   @Override
   public Builder visitCompileOption(CompileOptionContext ctx) {
     return createTreeFromFirstNode(ctx);
-  }
-
-  @Override
-  public Builder visitCompileLang(CompileLangContext ctx) {
-    return visitChildren(ctx);
   }
 
   @Override
@@ -2764,8 +2742,7 @@ public class JPNodeVisitor extends ProparseBaseVisitor<Builder> {
   // ------------------
 
   /**
-   * Default behavior for each ParseTree node is to create an array of JPNode. ANTLR2 construct ruleName: TOKEN TOKEN |
-   * rule TOKEN | rule ...
+   * Default behavior for each ParseTree node is to create an array of JPNode.
    */
   @Override
   @Nonnull

proparse/src/test/java/org/prorefactor/core/ParserTest.java
@@ -191,6 +191,14 @@ public class ParserTest {
   }
 
   @Test
+  public void testExpr() {
+    ParseUnit unit = new ParseUnit(new ByteArrayInputStream("message xx. catch err as Progress.Lang.Error: message err:GetClass():TypeName. end.".getBytes()), "<unnamed>", session);
+    unit.treeParser01();
+    assertFalse(unit.hasSyntaxError());
+    assertEquals(unit.getTopNode().queryStateHead(ABLNodeType.MESSAGE).size(), 2);
+  }
+
+  @Test
   public void testConnectDatabase() {
     ParseUnit unit = new ParseUnit(new ByteArrayInputStream("connect database dialog box".getBytes()), "<unnamed>", session);
     unit.parse();

proparse/src/main/antlr4/imports/BaseTokenTypes.tokens
@@ -116,7 +116,7 @@ User_func=191
 Widget_ref=192
 With_columns=193
 With_down=194
-
+Attribute_ref=195
 
 AACBIT=201
 AACONTROL=202

proparse/src/main/java/org/prorefactor/core/ABLNodeType.java
@@ -159,6 +159,7 @@ public enum ABLNodeType {
   FORMAT_PHRASE(Proparse.Format_phrase, NodeTypesOption.STRUCTURE),
   INLINE_DEFINITION(Proparse.Inline_definition, NodeTypesOption.STRUCTURE),
   LOCAL_METHOD_REF(Proparse.Local_method_ref, NodeTypesOption.STRUCTURE),
+  ATTRIBUTE_REF(Proparse.Attribute_ref, NodeTypesOption.STRUCTURE),
   LOOSE_END_KEEPER(Proparse.Loose_End_Keeper, NodeTypesOption.STRUCTURE),
   METHOD_PARAM_LIST(Proparse.Method_param_list, NodeTypesOption.STRUCTURE),
   METHOD_PARAMETER(Proparse.Method_parameter, NodeTypesOption.STRUCTURE),

proparse/src/main/java/org/prorefactor/treeparser/TreeParserComputeReferences.java
@@ -14,21 +14,21 @@
  ********************************************************************************/
 package org.prorefactor.treeparser;
 
+import org.antlr.v4.runtime.tree.ParseTree;
 import org.prorefactor.core.JPNode;
 import org.prorefactor.core.nodetypes.FieldRefNode;
 import org.prorefactor.core.nodetypes.RecordNameNode;
 import org.prorefactor.proparse.antlr4.Proparse.CatchStatementContext;
-import org.prorefactor.proparse.antlr4.Proparse.ColonAttributeContext;
+import org.prorefactor.proparse.antlr4.Proparse.ExprTermAttributeContext;
+import org.prorefactor.proparse.antlr4.Proparse.ExprTermMethodCallContext;
+import org.prorefactor.proparse.antlr4.Proparse.ExprTermOtherContext;
+import org.prorefactor.proparse.antlr4.Proparse.ExprTermWidgetContext;
 import org.prorefactor.proparse.antlr4.Proparse.Exprt2FieldContext;
-import org.prorefactor.proparse.antlr4.Proparse.ExprtExprt2Context;
-import org.prorefactor.proparse.antlr4.Proparse.ExprtWidNameContext;
 import org.prorefactor.proparse.antlr4.Proparse.FieldContext;
 import org.prorefactor.proparse.antlr4.Proparse.ParameterArgDatasetHandleContext;
 import org.prorefactor.proparse.antlr4.Proparse.ParameterArgTableHandleContext;
 import org.prorefactor.proparse.antlr4.Proparse.RecordContext;
 import org.prorefactor.proparse.antlr4.Proparse.WidNameContext;
-import org.prorefactor.proparse.antlr4.Proparse.WidattrExprt2Context;
-import org.prorefactor.proparse.antlr4.Proparse.WidattrWidNameContext;
 import org.prorefactor.treeparser.symbols.FieldBuffer;
 import org.prorefactor.treeparser.symbols.TableBuffer;
 import org.prorefactor.treeparser.symbols.Variable;
@@ -58,37 +58,24 @@ public class TreeParserComputeReferences extends AbstractBlockProparseListener {
   }
 
   @Override
-  public void enterExprtWidName(ExprtWidNameContext ctx) {
-    widattr(ctx.widName(), ctx.colonAttribute(), support.getNode(ctx), contextQualifiers.get(ctx));
-  }
+  public void enterExprTermAttribute(ExprTermAttributeContext ctx) {
+    ContextQualifier cq = contextQualifiers.get(ctx.id);
 
-  @Override
-  public void enterExprtExprt2(ExprtExprt2Context ctx) {
-    if ((ctx.colonAttribute() != null) && (ctx.expressionTerm2() instanceof Exprt2FieldContext)
-        && (ctx.colonAttribute().colonAttributeSub(0).OBJCOLON() != null)) {
-      widattr((Exprt2FieldContext) ctx.expressionTerm2(), contextQualifiers.get(ctx),
-          ctx.colonAttribute().colonAttributeSub(0).id.getText());
+    if (ctx.expressionTerm() instanceof ExprTermOtherContext) {
+      ExprTermOtherContext ctx2 = (ExprTermOtherContext) ctx.expressionTerm();
+      if (ctx2.expressionTerm2() instanceof Exprt2FieldContext) {
+        Exprt2FieldContext fld = (Exprt2FieldContext) ctx2.expressionTerm2();
+        widattr(fld, cq, ctx.id.getText());
+      }
+    } else if (ctx.expressionTerm() instanceof ExprTermWidgetContext) {
+      widattr(ctx, (ExprTermWidgetContext) ctx.expressionTerm(), cq, ctx.id.getText());
     }
-  }
-
-  @Override
-  public void enterWidattrExprt2(WidattrExprt2Context ctx) {
-    if ((ctx.expressionTerm2() instanceof Exprt2FieldContext)
-        && (ctx.colonAttribute().colonAttributeSub(0).id != null)) {
-      widattr((Exprt2FieldContext) ctx.expressionTerm2(), contextQualifiers.get(ctx),
-          ctx.colonAttribute().colonAttributeSub(0).id.getText());
-    }
-  }
-
-  @Override
-  public void enterWidattrWidName(WidattrWidNameContext ctx) {
-    widattr(ctx.widName(), ctx.colonAttribute(), support.getNode(ctx.getParent()), contextQualifiers.get(ctx));
   }
 
   @Override
   public void enterWidName(WidNameContext ctx) {
     if ((ctx.BUFFER() != null) || (ctx.TEMPTABLE() != null)) {
-      TableBuffer tableBuffer = currentScope.lookupBuffer(ctx.filn().getText());
+      TableBuffer tableBuffer = currentScope.lookupBuffer(ctx.identifier().getText());
       if (tableBuffer != null) {
         tableBuffer.noteReference(support.getNode(ctx), ContextQualifier.SYMBOL);
       }
@@ -133,26 +120,6 @@ public class TreeParserComputeReferences extends AbstractBlockProparseListener {
     }
   }
 
-  private void widattr(WidNameContext ctx, ColonAttributeContext ctx2, JPNode node, ContextQualifier cq) {
-    if ((ctx == null) || (ctx2 == null))
-      return;
-    if ((ctx.systemHandleName() != null) && (ctx.systemHandleName().THISOBJECT() != null)) {
-      FieldLookupResult result = rootScope.getRootBlock().lookupField(ctx2.colonAttributeSub(0).id.getText(), true);
-      if (result == null)
-        return;
-
-      if (result.getSymbol() instanceof Variable) {
-        result.getSymbol().noteReference(node, cq);
-        // If using chained expression, then we add a REF reference
-        if ((cq != ContextQualifier.REF) && (ctx2.colonAttributeSub().size() > 1)) {
-          result.getSymbol().noteReference(node, ContextQualifier.REF);
-        } else if (support.getNode(ctx) != null) { // Try to associate Symbol to WidgetRef object
-          support.getNode(ctx).getParent().setSymbol((Variable) result.getSymbol());
-        }
-      }
-    }
-  }
-
   // Called from expressionTerm rule (expressionTerm2 option) and widattr rule (widattrExprt2 option)
   // Tries to add references to variables/properties of current class, or references to static classes
   private void widattr(Exprt2FieldContext ctx2, ContextQualifier cq, String right) {
@@ -169,6 +136,24 @@ public class TreeParserComputeReferences extends AbstractBlockProparseListener {
       // Variable
       if (result.getSymbol() instanceof Variable) {
         result.getSymbol().noteReference(support.getNode(ctx2.getParent().getParent()), cq);
+      }
+    }
+  }
+
+  private void widattr(ParseTree ctx, ExprTermWidgetContext ctx2, ContextQualifier cq, String right) {
+    if ((ctx2.widName().systemHandleName() != null) && (ctx2.widName().systemHandleName().THISOBJECT() != null)) {
+      FieldLookupResult result = rootScope.getRootBlock().lookupField(right, true);
+      if (result == null)
+        return;
+      if (result.getSymbol() instanceof Variable) {
+        result.getSymbol().noteReference(support.getNode(ctx), cq);
+        // If using chained expression, then we add a REF reference
+        if ((cq != ContextQualifier.REF) && ((ctx.getParent() instanceof ExprTermMethodCallContext)
+            || (ctx.getParent() instanceof ExprTermAttributeContext))) {
+          result.getSymbol().noteReference(support.getNode(ctx), ContextQualifier.REF);
+        } else if (support.getNode(ctx) != null) {
+          support.getNode(ctx).setSymbol((Variable) result.getSymbol());
+        }
       }
     }
   }

proparse/src/main/java/org/prorefactor/treeparser/TreeParserVariableDefinition.java
@@ -385,34 +385,47 @@ public class TreeParserVariableDefinition extends AbstractBlockProparseListener 
 
   // Expression term
 
+  private void setStaticQualifier(ExpressionTermContext ctx) {
+    if (ctx instanceof ExprTermOtherContext) {
+      ExprTermOtherContext ctx2 = (ExprTermOtherContext) ctx;
+      if (ctx2.expressionTerm2() instanceof Exprt2FieldContext) {
+        Exprt2FieldContext fld = (Exprt2FieldContext) ctx2.expressionTerm2();
+        String clsRef = fld.getText();
+        if ((fld.ENTERED() == null) && !Strings.isNullOrEmpty(support.lookupClassName(clsRef))) {
+          FieldLookupResult result = currentBlock.lookupField(clsRef, true);
+          if (result == null)
+            setContextQualifier(ctx2, ContextQualifier.STATIC);
+        }
+      }
+    }
+  }
+
   @Override
-  public void enterExprtNoReturnValue(ExprtNoReturnValueContext ctx) {
+  public void enterExprTermMethodCall(ExprTermMethodCallContext ctx) {
     ContextQualifier qual = contextQualifiers.removeFrom(ctx);
-    setContextQualifier(ctx.sWidget(), qual);
-    setContextQualifier(ctx.colonAttribute(), qual);
+    setContextQualifier(ctx.expressionTerm(), ContextQualifier.REF);
+    setContextQualifier(ctx.id, qual);
+    setStaticQualifier(ctx.expressionTerm());
   }
 
   @Override
-  public void enterExprtExprt2(ExprtExprt2Context ctx) {
-    if ((ctx.colonAttribute() != null) && (ctx.expressionTerm2() instanceof Exprt2FieldContext)
-        && (ctx.colonAttribute().colonAttributeSub(0).OBJCOLON() != null)) {
-      ContextQualifier qual = contextQualifiers.get(ctx);
-      widattr((Exprt2FieldContext) ctx.expressionTerm2(), qual, ctx.colonAttribute().colonAttributeSub(0).id.getText());
-    } else {
-      ContextQualifier qual = contextQualifiers.removeFrom(ctx);
-      setContextQualifier(ctx.expressionTerm2(), qual);
-      if (ctx.colonAttribute() != null)
-        setContextQualifier(ctx.colonAttribute(), qual);
-    }
+  public void enterExprTermAttribute(ExprTermAttributeContext ctx) {
+    ContextQualifier qual = contextQualifiers.removeFrom(ctx);
+    setContextQualifier(ctx.expressionTerm(), ContextQualifier.REF);
+    setContextQualifier(ctx.id, qual);
+    setStaticQualifier(ctx.expressionTerm());
   }
 
   @Override
-  public void enterWidattrExprt2(WidattrExprt2Context ctx) {
-    if ((ctx.expressionTerm2() instanceof Exprt2FieldContext)
-        && (ctx.colonAttribute().colonAttributeSub(0).id != null)) {
-      widattr((Exprt2FieldContext) ctx.expressionTerm2(), contextQualifiers.get(ctx),
-          ctx.colonAttribute().colonAttributeSub(0).id.getText());
-    }
+  public void enterExprTermWidget(ExprTermWidgetContext ctx) {
+    ContextQualifier qual = contextQualifiers.removeFrom(ctx);
+    setContextQualifier(ctx.widName(), qual);
+  }
+
+  @Override
+  public void enterExprTermOther(ExprTermOtherContext ctx) {
+    ContextQualifier qual = contextQualifiers.removeFrom(ctx);
+    setContextQualifier(ctx.expressionTerm2(), qual);
   }
 
   @Override
@@ -482,8 +495,8 @@ public class TreeParserVariableDefinition extends AbstractBlockProparseListener 
   public void enterAssignStatement2(AssignStatement2Context ctx) {
     // Shorthand operator also read variable content
     ContextQualifier qual = ctx.EQUAL() == null ? ContextQualifier.REFUP : ContextQualifier.UPDATING;
-    if (ctx.widattr() != null) {
-      setContextQualifier(ctx.widattr(), qual);
+    if (ctx.expressionTerm() != null) {
+      setContextQualifier(ctx.expressionTerm(), qual);
     } else if (ctx.field() != null) {
       setContextQualifier(ctx.field(), qual);
     }
@@ -494,8 +507,8 @@ public class TreeParserVariableDefinition extends AbstractBlockProparseListener 
   public void enterAssignEqual(AssignEqualContext ctx) {
     // Shorthand operator also read variable content
     ContextQualifier qual = ctx.EQUAL() == null ? ContextQualifier.REFUP : ContextQualifier.UPDATING;
-    if (ctx.widattr() != null) {
-      setContextQualifier(ctx.widattr(), qual);
+    if (ctx.expressionTerm() != null) {
+      setContextQualifier(ctx.expressionTerm(), qual);
     } else if (ctx.field() != null) {
       setContextQualifier(ctx.field(), qual);
     }
@@ -1260,8 +1273,8 @@ public class TreeParserVariableDefinition extends AbstractBlockProparseListener 
 
   @Override
   public void enterFieldEqualDynamicNew(FieldEqualDynamicNewContext ctx) {
-    if (ctx.widattr() != null) {
-      setContextQualifier(ctx.widattr(), ContextQualifier.UPDATING);
+    if (ctx.expressionTerm() != null) {
+      setContextQualifier(ctx.expressionTerm(), ContextQualifier.UPDATING);
     } else if (ctx.field() != null) {
       setContextQualifier(ctx.field(), ContextQualifier.UPDATING);
     }
@@ -2287,19 +2300,6 @@ public class TreeParserVariableDefinition extends AbstractBlockProparseListener 
 
   private void defineWorktable(JPNode defAST, String name) {
     defineTable(defAST, name, IConstants.ST_WTABLE);
-  }
-
-  // Called from expressionTerm rule (expressionTerm2 option) and widattr rule (widattrExprt2 option)
-  // Tries to add references to variables/properties of current class, or references to static classes
-  private void widattr(Exprt2FieldContext ctx2, ContextQualifier cq, String right) {
-    String clsRef = ctx2.field().getText();
-
-    if ((ctx2.ENTERED() == null) && !Strings.isNullOrEmpty(support.lookupClassName(clsRef))) {
-      // First check if there's a variable name by this name
-      FieldLookupResult result = currentBlock.lookupField(clsRef, true);
-      if (result == null)
-        setContextQualifier(ctx2, ContextQualifier.STATIC);
-    }
   }
 
   private void frameRef(JPNode idAST) {

proparse/src/test/java/org/prorefactor/core/TreeParser03Test.java
@@ -513,7 +513,7 @@ public class TreeParser03Test {
     assertEquals(var1.getNumWrites(), 1);
     Variable var2 = unit.getRootScope().getVariable("yyy");
     assertEquals(var2.getNumReads(), 1);
-    assertEquals(var2.getNumWrites(), 2);
+    assertEquals(var2.getNumWrites(), 1);
     
     assertEquals(unit.getRootScope().getChildScopes().size(), 2);
     Variable var3 = unit.getRootScope().getChildScopes().get(0).getVariable("xxx");
@@ -524,16 +524,16 @@ public class TreeParser03Test {
     JPNode fooBlock = unit.getRootScope().getChildScopes().get(0).getRootBlock().getNode().findDirectChild(
         ABLNodeType.CODE_BLOCK);
     assertEquals(fooBlock.getDirectChildren().get(0).getFirstChild().getFirstChild().getNodeType(),
-        ABLNodeType.WIDGET_REF);
+        ABLNodeType.ATTRIBUTE_REF);
     assertEquals(fooBlock.getDirectChildren().get(0).getFirstChild().getFirstChild().getSymbol(), var1);
 
     JPNode barBlock = unit.getRootScope().getChildScopes().get(1).getRootBlock().getNode().findDirectChild(
         ABLNodeType.CODE_BLOCK);
     assertEquals(barBlock.getDirectChildren().get(0).getFirstChild().getFirstChild().getNodeType(),
-        ABLNodeType.WIDGET_REF);
+        ABLNodeType.ATTRIBUTE_REF);
     assertEquals(barBlock.getDirectChildren().get(0).getFirstChild().getFirstChild().getSymbol(), var2);
     assertEquals(barBlock.getDirectChildren().get(1).getFirstChild().getFirstChild().getNodeType(),
-        ABLNodeType.WIDGET_REF);
+        ABLNodeType.ATTRIBUTE_REF);
     assertNull(barBlock.getDirectChildren().get(1).getFirstChild().getFirstChild().getSymbol());
   }
 

commit 18b064a6002c7313b17eba5146e5b03242c1d7f0

Fix regression introduced on superStatement in c7f4bd0

proparse/src/main/antlr4/org/prorefactor/proparse/antlr4/Proparse.g4
@@ -3221,7 +3221,7 @@ stopStatement:
   ;
 
 superStatement:
-    SUPER LEFTPAREN ( parameter ( COMMA parameter )* )? RIGHTPAREN
+    SUPER LEFTPAREN ( parameter ( COMMA parameter )* )? RIGHTPAREN statementEnd
   ;
 
 streamNameOrHandle:

proparse/src/main/java/org/prorefactor/proparse/JPNodeVisitor.java
@@ -2425,6 +2425,11 @@ public class JPNodeVisitor extends ProparseBaseVisitor<Builder> {
   }
 
   @Override
+  public Builder visitSuperStatement(SuperStatementContext ctx) {
+    return createStatementTreeFromFirstNode(ctx);
+  }
+
+  @Override
   public Builder visitStreamNameOrHandle(StreamNameOrHandleContext ctx) {
     return createTreeFromFirstNode(ctx);
   }

proparse/src/test/java/org/prorefactor/core/ParserTest.java
@@ -490,6 +490,16 @@ public class ParserTest {
   }
 
   @Test
+  public void testSuperStatement() {
+    ParseUnit unit = new ParseUnit(new ByteArrayInputStream("class foo: constructor foo(): super(). end. end class.".getBytes()),
+        "<unnamed>", session);
+    unit.treeParser01();
+    assertFalse(unit.hasSyntaxError());
+    assertEquals(unit.getTopNode().queryStateHead(ABLNodeType.SUPER).size(), 1);
+    assertEquals(unit.getTopNode().queryStateHead(ABLNodeType.PERIOD).size(), 0);
+  }
+
+  @Test
   public void testRecordFunction01() {
     ParseUnit unit = new ParseUnit(new ByteArrayInputStream(
         "find first _file. display recid(_file).".getBytes()),