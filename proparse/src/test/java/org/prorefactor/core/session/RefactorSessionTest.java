cb54e8ff7dea8b928209a603957bd46dd8a4df10
Gilles Querret <g.querret@riverside-software.fr>
2021-10-20 18:01:50 +0200

    Unused import

--- proparse/src/test/java/org/prorefactor/core/session/RefactorSessionTest.java
+++ proparse/src/test/java/org/prorefactor/core/session/RefactorSessionTest.java
@@ -19,7 +19,6 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertNull;
-import static org.testng.Assert.assertTrue;
 
 import org.prorefactor.core.util.UnitTestModule;
 import org.prorefactor.refactor.RefactorSession;
