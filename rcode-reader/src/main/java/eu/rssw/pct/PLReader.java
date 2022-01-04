Riverside-Software/sonar-openedge

commit 578e8d26c374907e23e8db16f1ef4b7078eb17d8

Replace java.io.File with java.nio.Path + better FileChannel implementation + backport PCT issue with 12.5 PL files

rcode-reader/src/main/java/eu/rssw/pct/PLReader.java
@@ -19,23 +19,20 @@
  */
 package eu.rssw.pct;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.RandomAccessFile;
-
 import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
+import java.nio.channels.SeekableByteChannel;
 import java.nio.charset.Charset;
-
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Class for reading and extracting contents of a Progress Library file.
- * 
- * @author <a href="mailto:g.querret+PCT@gmail.com">Gilles QUERRET</a>
+ * Read and extract content of a Progress Library file.
  */
 public class PLReader {
   private static final int MAGIC_V11 = 0xd70b;
@@ -43,26 +40,20 @@ public class PLReader {
   private static final int ENCODING_SIZE = 20;
   private static final int FILE_LIST_OFFSET_V11 = 0x22;
 
-  private File pl;
+  private Path library;
   private List<FileEntry> files = null;
 
-  public PLReader(File file) {
-    String name = file.getPath();
-    SecurityManager sm = System.getSecurityManager();
-    if (sm != null) {
-      sm.checkRead(name);
-    }
-
-    this.pl = file;
+  public PLReader(Path file) {
+    this.library = file;
   }
 
   /**
-   * Returns entries contained in this procedure library
+   * Return entries contained in this procedure library
    * 
    * @throws RuntimeException If file is not a valid procedure library
    */
   public List<FileEntry> getFileList() {
-    if (this.files == null)
+    if (files == null)
       readFileList();
     return files;
   }
@@ -77,18 +68,17 @@ public class PLReader {
   }
 
   private void readFileList() {
-    try (RandomAccessFile raf = new RandomAccessFile(pl, "r")) {
-      FileChannel fc = raf.getChannel();
+    try (SeekableByteChannel channel = Files.newByteChannel(library, StandardOpenOption.READ)) {
       ByteBuffer magic = ByteBuffer.allocate(2);
-      fc.read(magic);
+      channel.read(magic);
       if ((magic.getShort(0) & 0xffff) != MAGIC_V11)
         throw new RuntimeException("Not a valid PL file");
 
-      Charset charset = getCharset(fc);
-      int offset = getTOCOffset(fc);
+      Charset charset = getCharset(channel);
+      int offset = getTOCOffset(channel);
       files = new ArrayList<>();
       FileEntry fe = null;
-      while ((fe = readEntry(fc, offset, charset)) != null) {
+      while ((fe = readEntry(channel, offset, charset)) != null) {
         if (fe.isValid())
           files.add(fe);
         offset += fe.getTocSize();
@@ -98,77 +88,100 @@ public class PLReader {
     }
   }
 
-  public InputStream getInputStream(FileEntry fe) throws IOException {
-    ByteBuffer bb = null;
-    try (RandomAccessFile raf = new RandomAccessFile(pl, "r")) {
-      FileChannel fc = raf.getChannel();
-      bb = ByteBuffer.allocate(fe.getSize());
-      fc.read(bb, fe.getOffset());
+  public InputStream getInputStream(FileEntry entry) throws IOException {
+    ByteBuffer buf = null;
+    try (SeekableByteChannel channel = Files.newByteChannel(library, StandardOpenOption.READ)) {
+      buf = ByteBuffer.allocate(entry.getSize());
+      channel.position(entry.getOffset());
+      channel.read(buf);
     }
 
-    return new ByteArrayInputStream(bb.array());
+    return new ByteBufferBackedInputStream(buf.position(0));
   }
 
-  private Charset getCharset(FileChannel fc) throws IOException {
-    ByteBuffer bEncoding = ByteBuffer.allocate(ENCODING_SIZE);
-    if (fc.read(bEncoding, ENCODING_OFFSET) != ENCODING_SIZE)
+  private Charset getCharset(SeekableByteChannel channel) throws IOException {
+    ByteBuffer buf = ByteBuffer.allocate(ENCODING_SIZE);
+    channel.position(ENCODING_OFFSET);
+    if (channel.read(buf) != ENCODING_SIZE)
       throw new RuntimeException("Invalid PL file");
-    bEncoding.position(0);
+
     StringBuilder sbEncoding = new StringBuilder();
     int zz = 0;
-    while ((zz < 20) && (bEncoding.get(zz) != 0)) {
-      sbEncoding.append((char) bEncoding.get(zz++));
+    while ((zz < ENCODING_SIZE) && (buf.get(zz) != 0)) {
+      sbEncoding.append((char) buf.get(zz++));
     }
     try {
       return Charset.forName(sbEncoding.toString());
     } catch (IllegalArgumentException iae) {
-      return Charset.forName("US-ASCII");
+      return StandardCharsets.US_ASCII;
     }
   }
 
-  private int getTOCOffset(FileChannel fc) throws IOException {
-    ByteBuffer bTOC = ByteBuffer.allocate(4);
-    if (fc.read(bTOC, FILE_LIST_OFFSET_V11) != 4)
+  private int getTOCOffset(SeekableByteChannel channel) throws IOException {
+    ByteBuffer buf = ByteBuffer.allocate(4);
+    channel.position(FILE_LIST_OFFSET_V11);
+    if (channel.read(buf) != 4)
       throw new RuntimeException("Invalid PL file");
-    return bTOC.getInt(0);
+
+    return buf.getInt(0);
   }
 
-  private FileEntry readEntry(FileChannel fc, int offset, Charset charset) throws IOException {
-    ByteBuffer b1 = ByteBuffer.allocate(1);
-    fc.read(b1, offset);
+  private FileEntry readEntry(SeekableByteChannel channel, int offset, Charset charset) throws IOException {
+    ByteBuffer buf1 = ByteBuffer.allocate(1);
+    channel.position(offset);
+    channel.read(buf1);
 
-    if (b1.get(0) == (byte) 0xFE) {
-      boolean stop = false;
-      int zz = 0;
-      while (!stop) {
-        b1.position(0);
-        int kk = fc.read(b1, (long) offset + ++zz);
-        stop = (kk == -1) || (b1.get(0) == (byte) 0xFF);
+    if (buf1.get(0) == (byte) 0xFE) {
+      while ((channel.read(buf1.position(0)) != -1) && (buf1.get(0) != (byte) 0xFF)) {
+        // Just read until EOF or next 0xFF
       }
-
-      return new FileEntry(zz);
-    } else if (b1.get(0) == (byte) 0xFF) {
-      b1.position(0);
-      fc.read(b1, (long) offset + 1);
-      int fNameSize = (int) b1.get(0) & 0xFF;
+      return new FileEntry((int) (channel.position() - offset - 1));
+    } else if (buf1.get(0) == (byte) 0xFF) {
+      channel.read(buf1.position(0));
+      int fNameSize = (int) buf1.get(0) & 0xFF;
       if (fNameSize == 0)
-        return new FileEntry(29);
-      ByteBuffer b2 = ByteBuffer.allocate(fNameSize);
-      fc.read(b2, (long) offset + 2);
-      b2.position(0);
-      String fName = charset.decode(b2).toString();
-      ByteBuffer b3 = ByteBuffer.allocate(48); // Ou 47
-      fc.read(b3, (long) offset + 2 + fNameSize);
-      int fileOffset = b3.getInt(6); // 7
-      int fileSize = b3.getInt(11); // 12
-      long added = b3.getInt(15) * 1000L; // 16
-      long modified = b3.getInt(19) * 1000L; // 20
+        return new FileEntry(49);
 
-      int tocSize = (b3.get(47) == 0 ? 50 : 49) + fNameSize;
+      ByteBuffer buf2 = ByteBuffer.allocate(fNameSize);
+      channel.read(buf2);
+      String fName = charset.decode(buf2.position(0)).toString();
+
+      ByteBuffer buf3 = ByteBuffer.allocate(48);
+      channel.read(buf3);
+      int fileOffset = buf3.getInt(6);
+      int fileSize = buf3.getInt(11);
+      long added = buf3.getInt(15) * 1000L;
+      long modified = buf3.getInt(19) * 1000L;
+
+      int tocSize = (buf3.get(47) == 0 ? 50 : 49) + fNameSize;
       return new FileEntry(fName, modified, added, fileOffset, fileSize, tocSize);
     } else {
       return null;
     }
+  }
 
+  public class ByteBufferBackedInputStream extends InputStream {
+    private ByteBuffer buf;
+
+    public ByteBufferBackedInputStream(ByteBuffer buf) {
+      this.buf = buf;
+    }
+
+    public int read() throws IOException {
+      if (!buf.hasRemaining()) {
+        return -1;
+      }
+      return buf.get() & 0xFF;
+    }
+
+    public int read(byte[] bytes, int off, int len) throws IOException {
+      if (!buf.hasRemaining()) {
+        return -1;
+      }
+
+      len = Math.min(len, buf.remaining());
+      buf.get(bytes, off, len);
+      return len;
+    }
   }
 }

rcode-reader/src/test/java/eu/rssw/pct/PLReaderTest.java
@@ -19,8 +19,8 @@
  */
 package eu.rssw.pct;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -31,7 +31,7 @@ public class PLReaderTest {
 
   @Test
   public void testRCodeInPL() throws IOException, InvalidRCodeException {
-    PLReader pl = new PLReader(new File("src/test/resources/ablunit.pl"));
+    PLReader pl = new PLReader(Path.of("src/test/resources/ablunit.pl"));
     Assert.assertNotNull(pl.getEntry("OpenEdge/ABLUnit/Reflection/ClassAnnotationInfo.r"));
     RCodeInfo rci = new RCodeInfo(pl.getInputStream(pl.getEntry("OpenEdge/ABLUnit/Reflection/ClassAnnotationInfo.r")));
     Assert.assertTrue(rci.isClass());

rcode-reader/src/test/java/eu/rssw/pct/RCodeInfoTest.java
@@ -25,8 +25,10 @@ import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertNull;
 import static org.testng.Assert.assertTrue;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.testng.annotations.Test;
 
@@ -55,7 +57,7 @@ public class RCodeInfoTest {
   }
 
   public void testEnum(String fileName) throws IOException {
-    try (FileInputStream input = new FileInputStream(fileName)) {
+    try (InputStream input = Files.newInputStream(Path.of(fileName))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
       assertNotNull(rci.getTypeInfo());
@@ -70,7 +72,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testInterface() throws IOException {
-    try (FileInputStream input = new FileInputStream("src/test/resources/rcode/IMyTest.r")) {
+    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/IMyTest.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
     } catch (InvalidRCodeException caught) {
@@ -80,7 +82,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testClass() throws IOException {
-    try (FileInputStream input = new FileInputStream("src/test/resources/rcode/BackupDataCallback.r")) {
+    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/BackupDataCallback.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
     } catch (InvalidRCodeException caught) {
@@ -90,7 +92,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testClass2() throws IOException {
-    try (FileInputStream input = new FileInputStream("src/test/resources/rcode/propList.r")) {
+    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/propList.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
       ITypeInfo info = rci.getTypeInfo();
@@ -130,7 +132,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testClass3() throws IOException {
-    try (FileInputStream input = new FileInputStream("src/test/resources/rcode/ttClass.r")) {
+    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/ttClass.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
     } catch (InvalidRCodeException caught) {
@@ -140,7 +142,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testClassMinSize() throws IOException {
-    try (FileInputStream input = new FileInputStream("src/test/resources/rcode/ClassMinSize.r")) {
+    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/ClassMinSize.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
       assertEquals(rci.getTypeInfo().getProperties().size(), 2);
@@ -151,7 +153,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testProcedure() throws IOException {
-    try (FileInputStream input = new FileInputStream("src/test/resources/rcode/compile.r")) {
+    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/compile.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertFalse(rci.isClass());
     } catch (InvalidRCodeException caught) {
@@ -161,7 +163,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testProcedure2() throws IOException {
-    try (FileInputStream input = new FileInputStream("src/test/resources/rcode/AbstractTTCollection.r")) {
+    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/AbstractTTCollection.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
     } catch (InvalidRCodeException caught) {
@@ -171,7 +173,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testProcedure3() throws IOException {
-    try (FileInputStream input = new FileInputStream("src/test/resources/rcode/FileTypeRegistry.r")) {
+    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/FileTypeRegistry.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
     } catch (InvalidRCodeException caught) {
@@ -181,7 +183,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testProcedure4() throws IOException {
-    try (FileInputStream input = new FileInputStream("src/test/resources/rcode/_dmpincr.r")) {
+    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/_dmpincr.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertFalse(rci.isClass());
     } catch (InvalidRCodeException caught) {
@@ -191,7 +193,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testV11() throws IOException {
-    try (FileInputStream input = new FileInputStream("src/test/resources/rcode/WebRequestV11.r")) {
+    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/WebRequestV11.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
       assertEquals(rci.getVersion(), 1100);
@@ -213,7 +215,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testV12() throws IOException {
-    try (FileInputStream input = new FileInputStream("src/test/resources/rcode/WebRequestV12.r")) {
+    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/WebRequestV12.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
       assertEquals(rci.getVersion(), -1215);
@@ -235,7 +237,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testV121() throws IOException {
-    try (FileInputStream input = new FileInputStream("src/test/resources/rcode/NMSTrace.r")) {
+    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/NMSTrace.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
       assertNotNull(rci.getTypeInfo());
@@ -247,7 +249,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testPackageProtected() throws IOException {
-    try (FileInputStream input = new FileInputStream("src/test/resources/rcode/PkgLevelAttr.r")) {
+    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/PkgLevelAttr.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
       assertNotNull(rci.getTypeInfo());
@@ -270,7 +272,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testTempTable() throws IOException {
-    try (FileInputStream input = new FileInputStream("src/test/resources/rcode/TempTableAttrs.r")) {
+    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/TempTableAttrs.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
       assertNotNull(rci.getTypeInfo());
@@ -305,7 +307,7 @@ public class RCodeInfoTest {
   }
 
   public void testElements(String fileName) throws IOException {
-    try (FileInputStream input = new FileInputStream(fileName)) {
+    try (InputStream input = Files.newInputStream(Path.of(fileName))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
       assertNotNull(rci.getTypeInfo());
@@ -419,7 +421,8 @@ public class RCodeInfoTest {
           testMethod4 = elem;
       }
       assertNotNull(testMethod4);
-      assertEquals(testMethod4.getSignature(), "testMethod4(IC,MLProgress.Lang.Object,OD,IDT,IDTZ,ODE,IH,I64,IB,ILC,OM,IRAW,IREC,IROW)");
+      assertEquals(testMethod4.getSignature(),
+          "testMethod4(IC,MLProgress.Lang.Object,OD,IDT,IDTZ,ODE,IH,I64,IB,ILC,OM,IRAW,IREC,IROW)");
       assertEquals(testMethod4.getReturnType().getPrimitive(), PrimitiveDataType.INTEGER);
       assertNull(testMethod4.getReturnType().getClassName());
 
@@ -438,7 +441,7 @@ public class RCodeInfoTest {
   }
 
   public void testElements2(String fileName) throws IOException {
-    try (FileInputStream input = new FileInputStream(fileName)) {
+    try (InputStream input = Files.newInputStream(Path.of(fileName))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
       assertNotNull(rci.getTypeInfo());

openedge-plugin/src/main/java/org/sonar/plugins/openedge/foundation/OpenEdgeSettings.java
@@ -348,7 +348,7 @@ public class OpenEdgeSettings {
 
   private void parseLibrary(File lib) {
     LOG.debug("Parsing PL {}", lib.getAbsolutePath());
-    PLReader pl = new PLReader(lib);
+    PLReader pl = new PLReader(lib.toPath());
     for (FileEntry entry : pl.getFileList()) {
       if (entry.getFileName().endsWith(".r")) {
         try {

commit 7736ffc00818b7403b64cfe49993caae8bc00447

Keep compatibility with Java 8
Commit 578e8d2 broke Java 8 compat

rcode-reader/src/main/java/eu/rssw/pct/PLReader.java
@@ -96,7 +96,7 @@ public class PLReader {
       channel.read(buf);
     }
 
-    return new ByteBufferBackedInputStream(buf.position(0));
+    return new ByteBufferBackedInputStream((ByteBuffer) buf.position(0));
   }
 
   private Charset getCharset(SeekableByteChannel channel) throws IOException {
@@ -132,19 +132,19 @@ public class PLReader {
     channel.read(buf1);
 
     if (buf1.get(0) == (byte) 0xFE) {
-      while ((channel.read(buf1.position(0)) != -1) && (buf1.get(0) != (byte) 0xFF)) {
+      while ((channel.read((ByteBuffer) buf1.position(0)) != -1) && (buf1.get(0) != (byte) 0xFF)) {
         // Just read until EOF or next 0xFF
       }
       return new FileEntry((int) (channel.position() - offset - 1));
     } else if (buf1.get(0) == (byte) 0xFF) {
-      channel.read(buf1.position(0));
+      channel.read((ByteBuffer) buf1.position(0));
       int fNameSize = (int) buf1.get(0) & 0xFF;
       if (fNameSize == 0)
         return new FileEntry(49);
 
       ByteBuffer buf2 = ByteBuffer.allocate(fNameSize);
       channel.read(buf2);
-      String fName = charset.decode(buf2.position(0)).toString();
+      String fName = charset.decode((ByteBuffer) buf2.position(0)).toString();
 
       ByteBuffer buf3 = ByteBuffer.allocate(48);
       channel.read(buf3);
@@ -167,6 +167,7 @@ public class PLReader {
       this.buf = buf;
     }
 
+    @Override
     public int read() throws IOException {
       if (!buf.hasRemaining()) {
         return -1;
@@ -174,6 +175,7 @@ public class PLReader {
       return buf.get() & 0xFF;
     }
 
+    @Override
     public int read(byte[] bytes, int off, int len) throws IOException {
       if (!buf.hasRemaining()) {
         return -1;

rcode-reader/src/test/java/eu/rssw/pct/PLReaderTest.java
@@ -23,7 +23,7 @@ import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
 
 import java.io.IOException;
-import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.testng.annotations.Test;
 
@@ -33,7 +33,7 @@ public class PLReaderTest {
 
   @Test
   public void testRCodeInPL() throws IOException, InvalidRCodeException {
-    PLReader pl = new PLReader(Path.of("src/test/resources/ablunit.pl"));
+    PLReader pl = new PLReader(Paths.get("src/test/resources/ablunit.pl"));
     assertNotNull(pl.getEntry("OpenEdge/ABLUnit/Reflection/ClassAnnotationInfo.r"));
     RCodeInfo rci = new RCodeInfo(pl.getInputStream(pl.getEntry("OpenEdge/ABLUnit/Reflection/ClassAnnotationInfo.r")));
     assertTrue(rci.isClass());

rcode-reader/src/test/java/eu/rssw/pct/RCodeInfoTest.java
@@ -28,12 +28,11 @@ import static org.testng.Assert.assertTrue;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.testng.annotations.Test;
 
 import eu.rssw.pct.RCodeInfo.InvalidRCodeException;
-import eu.rssw.pct.elements.PrimitiveDataType;
 import eu.rssw.pct.elements.DataType;
 import eu.rssw.pct.elements.IDatasetElement;
 import eu.rssw.pct.elements.IEventElement;
@@ -43,6 +42,7 @@ import eu.rssw.pct.elements.ITableElement;
 import eu.rssw.pct.elements.ITypeInfo;
 import eu.rssw.pct.elements.ParameterMode;
 import eu.rssw.pct.elements.ParameterType;
+import eu.rssw.pct.elements.PrimitiveDataType;
 
 public class RCodeInfoTest {
 
@@ -57,7 +57,7 @@ public class RCodeInfoTest {
   }
 
   public void testEnum(String fileName) throws IOException {
-    try (InputStream input = Files.newInputStream(Path.of(fileName))) {
+    try (InputStream input = Files.newInputStream(Paths.get(fileName))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
       assertNotNull(rci.getTypeInfo());
@@ -72,7 +72,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testInterface() throws IOException {
-    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/IMyTest.r"))) {
+    try (InputStream input = Files.newInputStream(Paths.get("src/test/resources/rcode/IMyTest.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
     } catch (InvalidRCodeException caught) {
@@ -82,7 +82,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testClass() throws IOException {
-    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/BackupDataCallback.r"))) {
+    try (InputStream input = Files.newInputStream(Paths.get("src/test/resources/rcode/BackupDataCallback.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
     } catch (InvalidRCodeException caught) {
@@ -92,7 +92,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testClass2() throws IOException {
-    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/propList.r"))) {
+    try (InputStream input = Files.newInputStream(Paths.get("src/test/resources/rcode/propList.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
       ITypeInfo info = rci.getTypeInfo();
@@ -132,7 +132,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testClass3() throws IOException {
-    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/ttClass.r"))) {
+    try (InputStream input = Files.newInputStream(Paths.get("src/test/resources/rcode/ttClass.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
     } catch (InvalidRCodeException caught) {
@@ -142,7 +142,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testClassMinSize() throws IOException {
-    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/ClassMinSize.r"))) {
+    try (InputStream input = Files.newInputStream(Paths.get("src/test/resources/rcode/ClassMinSize.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
       assertEquals(rci.getTypeInfo().getProperties().size(), 2);
@@ -153,7 +153,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testProcedure() throws IOException {
-    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/compile.r"))) {
+    try (InputStream input = Files.newInputStream(Paths.get("src/test/resources/rcode/compile.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertFalse(rci.isClass());
     } catch (InvalidRCodeException caught) {
@@ -163,7 +163,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testProcedure2() throws IOException {
-    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/AbstractTTCollection.r"))) {
+    try (InputStream input = Files.newInputStream(Paths.get("src/test/resources/rcode/AbstractTTCollection.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
     } catch (InvalidRCodeException caught) {
@@ -173,7 +173,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testProcedure3() throws IOException {
-    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/FileTypeRegistry.r"))) {
+    try (InputStream input = Files.newInputStream(Paths.get("src/test/resources/rcode/FileTypeRegistry.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
     } catch (InvalidRCodeException caught) {
@@ -183,7 +183,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testProcedure4() throws IOException {
-    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/_dmpincr.r"))) {
+    try (InputStream input = Files.newInputStream(Paths.get("src/test/resources/rcode/_dmpincr.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertFalse(rci.isClass());
     } catch (InvalidRCodeException caught) {
@@ -193,7 +193,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testV11() throws IOException {
-    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/WebRequestV11.r"))) {
+    try (InputStream input = Files.newInputStream(Paths.get("src/test/resources/rcode/WebRequestV11.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
       assertEquals(rci.getVersion(), 1100);
@@ -215,7 +215,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testV12() throws IOException {
-    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/WebRequestV12.r"))) {
+    try (InputStream input = Files.newInputStream(Paths.get("src/test/resources/rcode/WebRequestV12.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
       assertEquals(rci.getVersion(), -1215);
@@ -237,7 +237,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testV121() throws IOException {
-    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/NMSTrace.r"))) {
+    try (InputStream input = Files.newInputStream(Paths.get("src/test/resources/rcode/NMSTrace.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
       assertNotNull(rci.getTypeInfo());
@@ -249,7 +249,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testPackageProtected() throws IOException {
-    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/PkgLevelAttr.r"))) {
+    try (InputStream input = Files.newInputStream(Paths.get("src/test/resources/rcode/PkgLevelAttr.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
       assertNotNull(rci.getTypeInfo());
@@ -272,7 +272,7 @@ public class RCodeInfoTest {
 
   @Test
   public void testTempTable() throws IOException {
-    try (InputStream input = Files.newInputStream(Path.of("src/test/resources/rcode/TempTableAttrs.r"))) {
+    try (InputStream input = Files.newInputStream(Paths.get("src/test/resources/rcode/TempTableAttrs.r"))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
       assertNotNull(rci.getTypeInfo());
@@ -307,7 +307,7 @@ public class RCodeInfoTest {
   }
 
   public void testElements(String fileName) throws IOException {
-    try (InputStream input = Files.newInputStream(Path.of(fileName))) {
+    try (InputStream input = Files.newInputStream(Paths.get(fileName))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
       assertNotNull(rci.getTypeInfo());
@@ -441,7 +441,7 @@ public class RCodeInfoTest {
   }
 
   public void testElements2(String fileName) throws IOException {
-    try (InputStream input = Files.newInputStream(Path.of(fileName))) {
+    try (InputStream input = Files.newInputStream(Paths.get(fileName))) {
       RCodeInfo rci = new RCodeInfo(input);
       assertTrue(rci.isClass());
       assertNotNull(rci.getTypeInfo());