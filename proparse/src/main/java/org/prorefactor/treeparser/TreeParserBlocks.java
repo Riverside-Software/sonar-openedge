72ed54859ab73b0811c4f139006b1353459ef5c4
Gilles Querret <g.querret@riverside-software.fr>
2022-11-10 17:23:51 +0100

    Fix previous commit
    In some cases (specifically C3), ParseUnit object is not available

--- proparse/src/main/java/org/prorefactor/treeparser/TreeParserBlocks.java
+++ proparse/src/main/java/org/prorefactor/treeparser/TreeParserBlocks.java
@@ -196,8 +196,7 @@ public class TreeParserBlocks extends ProparseBaseListener {
 
   @Override
   public void enterConstructorStatement(ConstructorStatementContext ctx) {
-    newRoutine(ctx, support.getNode(ctx), unit.getClassName().substring(unit.getClassName().lastIndexOf('.') + 1),
-        ABLNodeType.CONSTRUCTOR);
+    newRoutine(ctx, support.getNode(ctx), ctx.tn.getText(), ABLNodeType.CONSTRUCTOR);
   }
 
   @Override
