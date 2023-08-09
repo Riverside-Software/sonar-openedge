1c6770f4c2d6073ea1c0777c01e1e9d7a3dc8eb2
Gilles Querret <g.querret@riverside-software.fr>
2023-08-09 12:13:09 +0200

    Add TypeInfoKryoProxy class

---
+++ rcode-reader/src/main/java/eu/rssw/pct/elements/fixed/TypeInfoKryoProxy.java
@@ -0,0 +1,53 @@
+/*
+ * OpenEdge plugin for SonarQube
+ * Copyright (c) 2015-2023 Riverside Software
+ * contact AT riverside DASH software DOT fr
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package eu.rssw.pct.elements.fixed;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+
+import eu.rssw.pct.elements.BuiltinClasses;
+import eu.rssw.pct.elements.ITypeInfo;
+
+public class TypeInfoKryoProxy extends TypeInfoProxy {
+  private final Kryo kryo;
+  private final Path path;
+
+  public TypeInfoKryoProxy(String typeName, Path path, Kryo kryo) {
+    super(typeName);
+    this.path = path;
+    this.kryo = kryo;
+  }
+
+  @Override
+  synchronized void checkTypeInfo() {
+    if (typeInfo != null)
+      return;
+    try {
+      typeInfo = (ITypeInfo) kryo.readClassAndObject(new Input(Files.readAllBytes(path)));
+    } catch (IOException caught) {
+      typeInfo = new TypeInfo(typeName, false, false, BuiltinClasses.PLO_CLASSNAME, "");
+    }
+  }
+
+}
