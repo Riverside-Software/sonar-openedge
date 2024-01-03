af8a8599d26fbba0c7c05496580832171d6523cf
Gilles Querret <g.querret@riverside-software.fr>
2024-01-03 09:56:43 +0100

    Deploy to vscode-abl

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,2 @@
 xcopy openedge-plugin\target\sonar-openedge-plugin-*.jar %SONAR_HOME%\extensions\downloads /y
+xcopy openedge-plugin\target\sonar-openedge-plugin-*.jar ..\vscode-abl\resources\sonar-openedge-plugin.jar /y
