Riverside-Software/sonar-openedge

commit 895f18e315ac39fad5ec429d626765c6ef8cc4a9

Handling of empty parameters in include files Pt. 2
Still issue #836

proparse/src/main/java/org/prorefactor/proparse/Lexer.java
@@ -1510,7 +1510,7 @@ public class Lexer implements IPreprocessor {
         ++cp.pos;
 
       // filename
-      String includeFilename = ppIncludeRefArg(cp);
+      String includeFilename = ppIncludeRefArg(cp, false);
 
       // whitespace?
       while (Character.isWhitespace(cp.chars[cp.pos]))
@@ -1549,7 +1549,7 @@ public class Lexer implements IPreprocessor {
               ++cp.pos;
             // Arg val
             if (cp.pos != closingCurly)
-              argVal = ppIncludeRefArg(cp);
+              argVal = ppIncludeRefArg(cp, false);
           }
 
           // Add the argument name/val pair
@@ -1570,7 +1570,7 @@ public class Lexer implements IPreprocessor {
           // Are we at closing curly?
           if (cp.pos == closingCurly)
             break;
-          incArgs.add(new IncludeArg("", ppIncludeRefArg(cp)));
+          incArgs.add(new IncludeArg("", ppIncludeRefArg(cp, true)));
         }
       } // numbered args
 
@@ -1670,14 +1670,18 @@ public class Lexer implements IPreprocessor {
    * A doublequote will start a string - all this means is that we'll collect whitespace. A singlequote does not have this effect.
    * If not a doublequote, we collect characters until we find a whitespace
    */
-  private String ppIncludeRefArg(MacroCharPos cp) {
+  private String ppIncludeRefArg(MacroCharPos cp, boolean numberedArg) {
     StringBuilder retVal = new StringBuilder();
     boolean gobbleWS = false;
-    char c = cp.chars[cp.pos++];
-    if (c == '"') {
-      gobbleWS = true;
-    } else {
-      retVal.append(c);
+    char c = cp.chars[cp.pos];
+    
+    if (!numberedArg) {
+      if (c == '"') {
+        gobbleWS = true;
+      } else {
+        retVal.append(c);
+      }
+      cp.pos++;
     }
 
     // Iterate up to, but not including, closing curly

proparse/src/test/java/org/prorefactor/core/PreprocessorDirectiveTest.java
@@ -403,6 +403,26 @@ public class PreprocessorDirectiveTest {
     assertEquals(tok.getText(), "\"    XXX BAR BAR XXX   test \"");
     tok = (ProToken) src.nextToken();
     assertEquals(tok.getNodeType(), ABLNodeType.PERIOD);
+    tok = nextVisibleToken(src);
+    assertEquals(tok.getNodeType(), ABLNodeType.MESSAGE);
+    tok = nextVisibleToken(src);
+    assertEquals(tok.getNodeType(), ABLNodeType.QSTRING);
+    assertEquals(tok.getText(), "\"\"");
+    tok = nextVisibleToken(src);
+    assertEquals(tok.getNodeType(), ABLNodeType.QSTRING);
+    assertEquals(tok.getText(), "\"value1\"");
+    tok = nextVisibleToken(src);
+    assertEquals(tok.getNodeType(), ABLNodeType.QSTRING);
+    assertEquals(tok.getText(), "\"value2\"");
+    tok = nextVisibleToken(src);
+    assertEquals(tok.getNodeType(), ABLNodeType.QSTRING);
+    assertEquals(tok.getText(), "\"value3\":U");
+    tok = nextVisibleToken(src);
+    assertEquals(tok.getNodeType(), ABLNodeType.QSTRING);
+    assertEquals(tok.getText(), "\"value4\"");
+    tok = nextVisibleToken(src);
+    assertEquals(tok.getNodeType(), ABLNodeType.QSTRING);
+    assertEquals(tok.getText(), "' '");
   }
 
   /**

proparse/src/test/resources/data/preprocessor/preprocessor20-3.i
@@ -1,0 +1,2 @@
+MESSAGE "{1} "{2}" "{3}" {4} {5} '{6}'.
+ 

proparse/src/test/resources/data/preprocessor/preprocessor20.p
@@ -6,3 +6,4 @@
       &param4 = "{ preprocessor/preprocessor20-2.i {&FOO}}"
       &param5 = " test "
       }
+{ preprocessor/preprocessor20-3.i "" "value1" value2 ""value3"":U ""value4"" " " }

commit 64779bfc2f5eb0971dfc344e7167a9bcf795f466

Fix parsing of double quotes in include file arguments
Issue #1142
Initial problem was introduced in 895f18e

proparse/src/main/java/org/prorefactor/proparse/Lexer.java
@@ -1759,22 +1759,21 @@ public class Lexer implements IPreprocessor {
    * If not a doublequote, we collect characters until we find a whitespace
    */
   private String ppIncludeRefArg(MacroCharPos cp, boolean numberedArg) {
-    StringBuilder retVal = new StringBuilder();
-    boolean gobbleWS = false;
-    char c = cp.chars[cp.pos];
-    
-    if (!numberedArg) {
-      if (c == '"') {
-        gobbleWS = true;
-      } else {
-        retVal.append(c);
-      }
-      cp.pos++;
+    // Handle empty string in double quotes followed by space in a different way depending on numberedArg value
+    if ((cp.pos <= cp.chars.length - 4) && (cp.chars[cp.pos] == '"') && (cp.chars[cp.pos + 1] == '"')
+        && Character.isWhitespace(cp.chars[cp.pos + 2])) {
+      cp.pos += 2;
+      if (numberedArg)
+        return "\"";
+      else
+        return "";
     }
 
+    StringBuilder retVal = new StringBuilder();
+    boolean gobbleWS = false;
     // Iterate up to, but not including, closing curly
     while (cp.pos < cp.chars.length - 1) {
-      c = cp.chars[cp.pos];
+      char c = cp.chars[cp.pos];
       switch (c) {
         case '"':
           if (cp.chars[cp.pos + 1] == '"') {

proparse/src/test/java/org/prorefactor/core/MacroGraphTest.java
@@ -28,6 +28,8 @@ import java.util.List;
 import org.prorefactor.core.util.SportsSchema;
 import org.prorefactor.core.util.UnitTestProparseSettings;
 import org.prorefactor.macrolevel.IncludeRef;
+import org.prorefactor.macrolevel.MacroDef;
+import org.prorefactor.macrolevel.MacroDefinitionType;
 import org.prorefactor.macrolevel.MacroEvent;
 import org.prorefactor.macrolevel.MacroRef;
 import org.prorefactor.macrolevel.NamedMacroRef;
@@ -225,4 +227,86 @@ public class MacroGraphTest extends AbstractProparseTest {
     assertNull(ref.getArgNumber(4));
   }
 
+  @Test
+  public void testIncludeParameter03() {
+    ParseUnit unit = getParseUnit(new File(SRC_DIR, "preprocessor20.p"), session);
+    unit.parse();
+    assertFalse(unit.hasSyntaxError());
+
+    List<MacroEvent> list = unit.getMacroGraph().findExternalMacroReferences();
+    assertNotNull(list);
+    assertEquals(list.size(), 3);
+
+    /* First include file */
+    IncludeRef ref1 = (IncludeRef) list.get(0);
+    assertEquals(ref1.getLine(), 6);
+    assertEquals(ref1.getEndLine(), 6);
+    assertEquals(ref1.getColumn(), 17);
+    assertEquals(ref1.getEndColumn(), 58);
+    assertEquals(ref1.numArgs(), 1);
+    MacroDef ref1Arg1 = ref1.getArgNumber(1);
+    assertEquals(ref1Arg1.getType(), MacroDefinitionType.NUMBEREDARG);
+    assertEquals(ref1Arg1.getValue(), "BAR");
+
+    /* Second include file */
+    IncludeRef ref2 = (IncludeRef) list.get(1);
+    assertEquals(ref2.getLine(), 2);
+    assertEquals(ref2.getEndLine(), 8);
+    assertEquals(ref2.getColumn(), 0);
+    assertEquals(ref2.getEndColumn(), 7);
+    assertEquals(ref2.numArgs(), 5);
+    MacroDef ref2Arg1 = ref2.lookupNamedArg("param1");
+    assertEquals(ref2Arg1.getValue(), "");
+    MacroDef ref2Arg2 = ref2.lookupNamedArg("param2");
+    assertEquals(ref2Arg2.getValue(), "");
+    MacroDef ref2Arg3 = ref2.lookupNamedArg("param3");
+    assertEquals(ref2Arg3.getValue(), " ");
+    MacroDef ref2Arg4 = ref2.lookupNamedArg("param4");
+    assertEquals(ref2Arg4.getValue(), "XXX BAR BAR XXX ");
+    MacroDef ref2Arg5 = ref2.lookupNamedArg("param5");
+    assertEquals(ref2Arg5.getValue(), " test ");
+
+    /* Third include file */
+    IncludeRef ref3 = (IncludeRef) list.get(2);
+    assertEquals(ref3.getLine(), 9);
+    assertEquals(ref3.getEndLine(), 9);
+    assertEquals(ref3.getColumn(), 0);
+    assertEquals(ref3.getEndColumn(), 82);
+    assertEquals(ref3.numArgs(), 6);
+    MacroDef ref3Arg1 = ref3.getArgNumber(1);
+    assertEquals(ref3Arg1.getValue(), "\"");
+    MacroDef ref3Arg2 = ref3.getArgNumber(2);
+    assertEquals(ref3Arg2.getValue(), "value1");
+    MacroDef ref3Arg3 = ref3.getArgNumber(3);
+    assertEquals(ref3Arg3.getValue(), "value2");
+    MacroDef ref3Arg4 = ref3.getArgNumber(4);
+    assertEquals(ref3Arg4.getValue(), "\"value3\":U");
+    MacroDef ref3Arg5 = ref3.getArgNumber(5);
+    assertEquals(ref3Arg5.getValue(), "\"value4\"");
+    MacroDef ref3Arg6 = ref3.getArgNumber(6);
+    assertEquals(ref3Arg6.getValue(), " ");
+  }
+
+  @Test
+  public void testIncludeParameter04() {
+    ParseUnit unit = getParseUnit(new File(SRC_DIR, "preprocessor25.p"), session);
+    unit.parse();
+    assertFalse(unit.hasSyntaxError());
+
+    List<MacroEvent> list = unit.getMacroGraph().findExternalMacroReferences();
+    assertNotNull(list);
+    assertEquals(list.size(), 1);
+
+    /* First include file */
+    IncludeRef ref1 = (IncludeRef) list.get(0);
+    assertEquals(ref1.getLine(), 1);
+    assertEquals(ref1.getEndLine(), 1);
+    assertEquals(ref1.getColumn(), 0);
+    assertEquals(ref1.getEndColumn(), 56);
+    assertEquals(ref1.numArgs(), 1);
+    MacroDef ref1Arg1 = ref1.getArgNumber(1);
+    assertEquals(ref1Arg1.getType(), MacroDefinitionType.NAMEDARG);
+    assertEquals(ref1Arg1.getName(), "DefaultValue");
+    assertEquals(ref1Arg1.getValue(), "\"-\"");
+  }
 }

proparse/src/test/resources/data/preprocessor/preprocessor25-01.i
@@ -1,0 +1,3 @@
+DEFINE VARIABLE xxx AS CHARACTER NO-UNDO.
+
+ASSIGN xxx = {&DefaultValue}.

proparse/src/test/resources/data/preprocessor/preprocessor25.p
@@ -1,0 +1,1 @@
+{ preprocessor/preprocessor25-01.i &DefaultValue=""-"" }