23faf04196fd5159dfde15f9f3f8fbdb7826c3d6
Gilles Querret <g.querret@riverside-software.fr>
2025-01-11 08:47:16 +0100

    Remove error-leading JPNode#isKeyword() method

--- proparse/src/main/java/org/prorefactor/core/JPNode.java
+++ proparse/src/main/java/org/prorefactor/core/JPNode.java
@@ -711,11 +711,6 @@ public class JPNode {
     return token.isAbbreviated();
   }
 
-  public boolean isKeyword() {
-    return token.getNodeType().isKeyword()
-        || ((token.getNodeType() == ABLNodeType.ID) && (ABLNodeType.isFormerUnreservedKeyword(getText())));
-  }
-
   /**
    * @return True if token is part of an editable section in AppBuilder managed code
    */
