4f4154fd1fca14e6318d6bcef50e640421a4c6d7
Gilles Querret <g.querret@riverside-software.fr>
2025-12-02 18:39:04 +0100

    Exclude compiler warnings from Sonarlint profile

---
+++ openedge-plugin/src/main/java/org/sonar/plugins/openedge/foundation/SonarLintDBProfile.java
@@ -0,0 +1,36 @@
+/*
+ * OpenEdge plugin for SonarQube
+ * Copyright (c) 2015-2025 Riverside Software
+ * contact AT riverside DASH software DOT fr
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.plugins.openedge.foundation;
+
+import org.sonar.api.server.profile.BuiltInQualityProfilesDefinition;
+import org.sonar.plugins.openedge.api.Constants;
+import org.sonar.plugins.openedge.checks.NoOpDatabaseRule;
+
+public class SonarLintDBProfile implements BuiltInQualityProfilesDefinition {
+  public static final String PROFILE_NAME = "Sonarlint way";
+
+  @Override
+  public void define(Context context) {
+    var profile = context.createBuiltInQualityProfile(PROFILE_NAME, Constants.DB_LANGUAGE_KEY).setDefault(true);
+    profile.activateRule(Constants.STD_DB_REPOSITORY_KEY, NoOpDatabaseRule.class.getName());
+    profile.done();
+  }
+
+}
