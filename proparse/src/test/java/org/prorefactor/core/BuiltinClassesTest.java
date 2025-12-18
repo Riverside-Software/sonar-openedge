a8a25dc723eb6db27dc913cfe358823c749da079
Cindy Delabrière <c.delabriere@riverside-software.fr>
2025-12-18 13:38:05 +0100

    Add Progress.Collections.* classes

--- proparse/src/test/java/org/prorefactor/core/BuiltinClassesTest.java
+++ proparse/src/test/java/org/prorefactor/core/BuiltinClassesTest.java
@@ -1,3 +1,17 @@
+/********************************************************************************
+ * Copyright (c) 2015-2025 Riverside Software
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU Lesser General Public License v3.0
+ * which is available at https://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR LGPL-3.0
+ ********************************************************************************/
 package org.prorefactor.core;
 
 import static org.testng.Assert.assertEquals;
@@ -48,6 +62,7 @@ public class BuiltinClassesTest {
     var provider117 = VERSION_TYPE_INFO_PROVIDER.apply(OpenEdgeVersion.V117);
     var provider122 = VERSION_TYPE_INFO_PROVIDER.apply(OpenEdgeVersion.V122);
     var provider128 = VERSION_TYPE_INFO_PROVIDER.apply(OpenEdgeVersion.V128);
+    var provider130 = VERSION_TYPE_INFO_PROVIDER.apply(OpenEdgeVersion.V130);
 
     var info1 = provider117.apply("Progress.IO.MemoryInputStream");
     assertNull(info1);
@@ -95,6 +110,15 @@ public class BuiltinClassesTest {
         it -> "Progress.Reflect.Property".equalsIgnoreCase(it.getO1().getTypeName())).toList();
     assertEquals(sub2.size(), 8);
     assertEquals(sub2.get(7).getO2().getName(), "IsFinal");
+
+    var info61 = provider117.apply("Progress.Collections.ICollection");
+    assertNull(info61);
+    var info62 = provider122.apply("Progress.Collections.ICollection");
+    assertNull(info62);
+    var info63 = provider128.apply("Progress.Collections.ICollection");
+    assertNotNull(info63);
+    var info64 = provider130.apply("Progress.Collections.ICollection");
+    assertNotNull(info64);
   }
 
   private static Consumer<ITypeInfo> classConsumer(BufferedWriter writer) {
@@ -145,7 +169,6 @@ public class BuiltinClassesTest {
     assertTrue(Files.exists(Path.of("target/dump/V122.txt")));
     assertTrue(Files.exists(Path.of("target/dump/V128.txt")));
     assertTrue(Files.exists(Path.of("target/dump/V130.txt")));
-    
   }
 
 }
