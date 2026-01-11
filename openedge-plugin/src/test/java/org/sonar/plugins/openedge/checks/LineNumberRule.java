9a2b47c1e474d242d774df35a474684e004b6c6c
Gilles Querret <g.querret@riverside-software.fr>
2026-01-11 17:00:33 +0100

    Copyright

--- openedge-plugin/src/test/java/org/sonar/plugins/openedge/checks/LineNumberRule.java
+++ openedge-plugin/src/test/java/org/sonar/plugins/openedge/checks/LineNumberRule.java
@@ -1,6 +1,6 @@
 /*
  * OpenEdge plugin for SonarQube
- * Copyright (c) 2019-2025 Riverside Software
+ * Copyright (c) 2019-2026 Riverside Software
  * contact AT riverside DASH software DOT fr
  * 
  * This program is free software; you can redistribute it and/or
