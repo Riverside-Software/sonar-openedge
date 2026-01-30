e01a579d091dc59c6f390e697d7f54812f94c5eb
Gilles Querret <g.querret@riverside-software.fr>
2026-01-30 12:13:00 +0100

    Comparators

--- proparse/src/main/java/org/prorefactor/core/schema/Constants.java
+++ proparse/src/main/java/org/prorefactor/core/schema/Constants.java
@@ -21,29 +21,14 @@ public class Constants {
   public static final IDatabase nullDatabase = new Database("");
   public static final ITable nullTable = new Table("");
 
-  /** Comparator for sorting by name. */
-  public static final Comparator<IDatabase> DB_NAME_ORDER = new Comparator<IDatabase>() {
-    @Override
-    public int compare(IDatabase d1, IDatabase d2) {
-      return d1.getName().compareToIgnoreCase(d2.getName());
-    }
-  };
+  // Sort databases by name
+  public static final Comparator<IDatabase> DB_NAME_ORDER = (d1, d2) -> d1.getName().compareToIgnoreCase(d2.getName());
 
-  /** Comparator for sorting by name. */
-  public static final Comparator<ITable> TABLE_NAME_ORDER = new Comparator<ITable>() {
-    @Override
-    public int compare(ITable t1, ITable t2) {
-      return t1.getName().compareToIgnoreCase(t2.getName());
-    }
-  };
+  // Sort tables by name
+  public static final Comparator<ITable> TABLE_NAME_ORDER = (t1, t2) -> t1.getName().compareToIgnoreCase(t2.getName());
 
-  /** Comparator for sorting by name. */
-  public static final Comparator<IField> FIELD_NAME_ORDER = new Comparator<IField>() {
-    @Override
-    public int compare(IField f1, IField f2) {
-      return f1.getName().compareToIgnoreCase(f2.getName());
-    }
-  };
+  // Sort fields by name
+  public static final Comparator<IField> FIELD_NAME_ORDER = (f1, f2) -> f1.getName().compareToIgnoreCase(f2.getName());
 
   private Constants() {
     // No-op
